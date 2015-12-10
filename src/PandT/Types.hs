bce18e7889c327995c962ba3740d2ac15792d755
Christopher Armstrong <radix@twistedmatrix.com>
2015-12-10 14:32:31 -0600

    implement some of the concretization of intensity
    to real values (Health and Damage)

--- src/PandT/Types.hs
+++ src/PandT/Types.hs
@@ -7,7 +7,13 @@ module PandT.Types where
 import Control.Lens
 import Data.Text
 
-data DamageSeverity = Low | Medium | High
+data Intensity = Low | Medium | High
+    deriving (Show, Eq, Ord)
+
+newtype DamageIntensity = DamageIntensity Intensity
+    deriving (Show, Eq, Ord)
+
+newtype Stamina = Stamina Intensity
     deriving (Show, Eq, Ord)
 
 newtype Range = Range Int
@@ -19,32 +25,54 @@ newtype Radius = Radius Int
 newtype Duration = Duration Int
     deriving (Show, Eq, Ord)
 
-newtype CastTime = CastTime Int
+newtype Period = Period Int
     deriving (Show, Eq, Ord)
 
-newtype Health = Health Int -- Or... hm.
+newtype CastTime = CastTime Int
     deriving (Show, Eq, Ord)
 
 newtype Cooldown = Cooldown Int
     deriving (Show, Eq, Ord)
 
+newtype Health = Health Int
+    deriving (Show, Eq, Ord)
 
 data Resource = Mana Int | Energy Int
     deriving (Show, Eq)
 
 
-data Condition
-    = DamageOverTime DamageSeverity Duration
-    | DamageAbsorb DamageSeverity Duration
-    | HealOverTime DamageSeverity Duration
+-- a condition
+-- 1. has a name
+-- 2. either has a duration or is infinite
+-- 3. has a summy "value":
+--    a. RecurringEffect Effect Period
+--    b. SomeThing
+--    c. OtherThing
+
+data ConditionDuration -- maybe add an Ord instance
+    = TimedCondition Duration
+    | UnlimitedDuration
     deriving (Show, Eq)
+
+data ConditionValue
+    = RecurringEffect Period Effect
+    | DamageAbsorb DamageIntensity
+    | DamageBuff DamageIntensity
+    deriving (Show, Eq)
+
+data Condition
+    = Condition
+    { _conditionName :: Text
+    , _conditionDuration :: ConditionDuration
+    , _conditionValue :: ConditionValue
+    } deriving (Show, Eq)
 
 
 data Effect
     = Interrupt
     | ApplyCondition Condition
-    | Heal DamageSeverity
-    | Damage DamageSeverity
+    | Heal DamageIntensity
+    | Damage DamageIntensity
     deriving (Show, Eq)
 
 
@@ -67,20 +95,29 @@ data Ability = Ability
 
 makeLenses ''Ability
 
-data AbilitySet = AbilitySet -- aka "class"
-    { abilities :: [Ability] }
-    deriving (Show, Eq)
-
-
 data Creature = Creature
     { _conditions :: [Condition]
     , _resource :: Resource
+    , _stamina :: Stamina
     , _health :: Health
-    , _abilitySets :: [AbilitySet]
+    , _abilities :: [Ability]
     }
     deriving (Show, Eq)
 
 makeLenses ''Creature
+
+_staminaToHealth :: Stamina -> Health
+_staminaToHealth (Stamina High) = Health 100
+_staminaToHealth (Stamina Medium) = Health 50
+_staminaToHealth (Stamina Low) = Health 25
+
+makeCreature :: Resource -> Stamina -> [Ability] -> Creature
+makeCreature res sta abs = Creature
+    { _conditions=[]
+    , _resource=res
+    , _stamina=sta
+    , _health=staminaToHealth sta
+    , _abilities=abs}
 
 data Combat = Combat
     { creatures :: [Creature]
@@ -88,20 +125,38 @@ data Combat = Combat
     deriving (Show, Eq)
 
 
+staminaToHealth :: Stamina -> Health
+staminaToHealth (Stamina High) = Health 100
+staminaToHealth (Stamina Medium) = Health 50
+staminaToHealth (Stamina Low) = Health 25
+
+damageToHealthVal :: DamageIntensity -> Int
+damageToHealthVal (DamageIntensity High) = 50
+damageToHealthVal (DamageIntensity Medium) = 25
+damageToHealthVal (DamageIntensity Low) = 10
+
+healthMinusDamage :: Health -> DamageIntensity -> Health
+healthMinusDamage (Health health) dmg = Health (health - (damageToHealthVal dmg))
+
+makeDotEffect :: Text -> Intensity -> ConditionDuration -> Effect
+makeDotEffect name int dur
+    = ApplyCondition
+        (Condition
+            { _conditionName=name
+            , _conditionValue=RecurringEffect (Period 1) (Damage (DamageIntensity int))
+            , _conditionDuration=dur})
+
+immolation = makeDotEffect "Immolation" Medium (TimedCondition (Duration 3))
+
 punch = Ability
     { _name="Punch"
     , _cost=Energy 10
-    , _effects=[Damage High, ApplyCondition (DamageOverTime Low (Duration 3))]
+    , _effects=[Damage (DamageIntensity High), immolation]
     , _target=TargetCreature (Range 1)
     , _castTime = CastTime 0
     , _cooldown = Cooldown 0
     }
-creat = Creature
-    { _conditions=[]
-    , _resource=Energy 100
-    , _health=Health 100
-    , _abilitySets=[AbilitySet [punch]]
-    }
+creat = makeCreature (Energy 100) (Stamina High) [punch]
 
 applyEffect :: Creature -> Effect -> Creature
 applyEffect creature (ApplyCondition condition) = over conditions (condition:) creature
