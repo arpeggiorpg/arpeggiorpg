e1d0cc24af8e49d7d1a8b0f37a60b5af3f3e7106
Christopher Armstrong <radix@twistedmatrix.com>
2017-01-04 16:50:48 -0600

    big simplification and improvement to nonempty

--- nonempty/src/lib.rs
+++ nonempty/src/lib.rs
@@ -9,95 +9,68 @@ extern crate serde;
 #[cfg(test)]
 extern crate serde_json;
 
-use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer};
 #[cfg(test)]
 use serde_json::error as SJE;
 
+
 /// A non-empty vector with a cursor. NO operations panic.
-/// Has Serde serialization implementations that serialize to e.g. `{"current": 0, "data": [...]}`
-// This should probably be refactored so that NonEmpty is a separate data type.
-#[derive(Clone, Eq, PartialEq, Debug)]
+/// Has Serde serialization implementations that serialize to e.g. `{"cursor": 0, "data": [...]}`
+#[derive(Clone, Eq, PartialEq, Debug, Serialize)]
 pub struct NonEmptyWithCursor<T> {
-    most: Vec<T>,
-    head: T,
     cursor: usize,
+    data: NonEmpty<T>,
 }
 
+
 impl<T> NonEmptyWithCursor<T> {
-    /// Construct a new NonEmptyWithCursor. The first element is necessary.
-    pub fn new(head: T) -> NonEmptyWithCursor<T> {
+    // *** Cursor methods
+    /// Create a new NonEmptyWithCursor with a single element and cursor set to 0.
+    pub fn new(head: T) -> Self {
         NonEmptyWithCursor {
-            most: vec![],
-            head: head,
             cursor: 0,
+            data: NonEmpty::new(head),
         }
     }
 
-    /// Iterate over the elements, providing &T.
-    pub fn iter(&self) -> NEIter<T> {
-        NEIter {
-            necurs: self,
-            current: 0,
+    /// Construct a new NonEmptyWithCursor from the first element and a vector of the rest of the
+    /// elements.
+    pub fn new_with_rest(head: T, rest: Vec<T>) -> Self {
+        NonEmptyWithCursor {
+            cursor: 0,
+            data: NonEmpty::new_with_rest(head, rest),
         }
-    }
-
-    /// Iterate over the elements, providing &mut T.
-    pub fn iter_mut(&mut self) -> NEIterMut<T> {
-        NEIterMut { iter: std::iter::once(&mut self.head).chain(self.most.iter_mut()) }
     }
 
     /// Get the current element, as determined by the cursor.
+    #[inline]
     pub fn get_current(&self) -> &T {
-        if self.cursor == 0 {
-            &self.head
-        } else {
-            &self.most[self.cursor - 1]
-        }
+        self.data.get(self.cursor).unwrap()
     }
 
     /// Get a mutable reference to the current element.
+    #[inline]
     pub fn get_current_mut(&mut self) -> &mut T {
         let i = self.cursor;
-        self.get_mut(i).unwrap()
-    }
-
-    /// Get an immutable reference to an arbitrary element, by index.
-    pub fn get(&self, idx: usize) -> Option<&T> {
-        if idx == 0 {
-            Some(&self.head)
-        } else {
-            self.most.get(idx - 1)
-        }
-    }
-
-    /// Get a mutable reference to an arbitrary element, by index.
-    pub fn get_mut(&mut self, idx: usize) -> Option<&mut T> {
-        if idx == 0 {
-            Some(&mut self.head)
-        } else {
-            self.most.get_mut(idx - 1)
-        }
-    }
-
-    /// Append an element.
-    pub fn push(&mut self, t: T) {
-        self.most.push(t)
+        self.data.get_mut(i).unwrap()
     }
 
     /// Set the cursor. Returns None if the cursor is out of bounds.
+    #[inline]
     pub fn set_cursor(&mut self, cursor: usize) -> Option<()> {
-        if self.most.len() < cursor {
-            None
-        } else {
+        if self.data.len() > cursor {
             self.cursor = cursor;
             Some(())
+        } else {
+            None
         }
     }
 
     /// Increment the cursor by one, and wrap around to 0 if it goes past the end of the vector.
+    #[inline]
     pub fn next_circular(&mut self) {
         let newcursor = self.cursor + 1;
-        self.cursor = if newcursor > self.most.len() {
+        self.cursor = if newcursor >= self.data.len() {
             0
         } else {
             newcursor
@@ -105,75 +78,127 @@ impl<T> NonEmptyWithCursor<T> {
     }
 
     /// Get the current cursor.
+    #[inline]
     pub fn get_cursor(&self) -> usize {
         self.cursor
     }
 
-    /// Return the total length.
+    // *** Pass-through methods
+    /// Get the length of the underlying non-empty vector.
+    #[inline]
     pub fn len(&self) -> usize {
-        1 + self.most.len()
+        self.data.len()
+    }
+
+    /// Iterate over the elements, providing &T.
+    #[inline]
+    pub fn iter(&self) -> std::slice::Iter<T> {
+        self.data.iter()
+    }
+
+    /// Iterate over the elements, providing &mut T.
+    #[inline]
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<T> {
+        self.data.iter_mut()
+    }
+
+    /// Get an immutable reference to an arbitrary element, by index.
+    #[inline]
+    pub fn get(&self, idx: usize) -> Option<&T> {
+        self.data.get(idx)
+    }
+
+    /// Get a mutable reference to an arbitrary element, by index.
+    #[inline]
+    pub fn get_mut(&mut self, idx: usize) -> Option<&mut T> {
+        self.data.get_mut(idx)
+    }
+
+    /// Append an element.
+    #[inline]
+    pub fn push(&mut self, t: T) {
+        self.data.push(t)
     }
 }
 
-impl<T> Serialize for NonEmptyWithCursor<T>
-    where T: Serialize
-{
-    fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-        where S: Serializer
-    {
-        // We have to make a helper struct to serialize the value of `data`, because we have to
-        // have something to pass to `serialize_map_value`.
-        struct Data<'a, T2>(&'a NonEmptyWithCursor<T2>) where T2: 'a;
-        impl<'a, T2> Serialize for Data<'a, T2>
-            where T2: Serialize
-        {
-            fn serialize<S2>(&self, serializer: &mut S2) -> Result<(), S2::Error>
-                where S2: Serializer
-            {
-                let mut state = serializer.serialize_seq(Some(self.0.len()))?;
-                serializer.serialize_seq_elt(&mut state, &self.0.head)?;
-                for elt in self.0.most.iter() {
-                    serializer.serialize_seq_elt(&mut state, elt)?;
-                }
-                serializer.serialize_seq_end(state)
-            }
+/// A non-empty vector. NO operations panic.
+// The canonical representation is something like (A, Vec<A>), but this layout actually makes
+// it MUCH easier to implement the various methods, and perhaps more optimized.
+#[derive(Clone, Eq, PartialEq, Debug, Serialize)]
+pub struct NonEmpty<T>(Vec<T>);
+
+impl<T> NonEmpty<T> {
+    /// Construct a new NonEmpty. The first element is necessary.
+    #[inline]
+    pub fn new(head: T) -> Self {
+        NonEmpty(vec![head])
+    }
+
+    /// Construct a new NonEmpty from the first element and a vector of the rest of the elements.
+    #[inline]
+    pub fn new_with_rest(head: T, rest: Vec<T>) -> Self {
+        let mut v = vec![head];
+        v.extend(rest);
+        NonEmpty(v)
+    }
+
+    /// Construct a new NonEmpty from a Vec, if it has at least one element.
+    #[inline]
+    pub fn from_vec(vec: Vec<T>) -> Option<Self> {
+        if vec.len() >= 1 {
+            Some(NonEmpty(vec))
+        } else {
+            None
         }
-        let mut state = serializer.serialize_map(Some(2))?;
-        serializer.serialize_map_key(&mut state, "cursor")?;
-        serializer.serialize_map_value(&mut state, self.cursor)?;
-        serializer.serialize_map_key(&mut state, "data")?;
-        serializer.serialize_map_value(&mut state, Data(&self))?;
-        serializer.serialize_map_end(state)
+    }
+
+    /// Iterate over the elements, providing &T.
+    #[inline]
+    pub fn iter(&self) -> std::slice::Iter<T> {
+        self.0.iter()
+    }
+
+    /// Iterate over the elements, providing &mut T.
+    #[inline]
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<T> {
+        self.0.iter_mut()
+    }
+
+    /// Get an immutable reference to an arbitrary element, by index.
+    #[inline]
+    pub fn get(&self, idx: usize) -> Option<&T> {
+        self.0.get(idx)
+    }
+
+    /// Get a mutable reference to an arbitrary element, by index.
+    #[inline]
+    pub fn get_mut(&mut self, idx: usize) -> Option<&mut T> {
+        self.0.get_mut(idx)
+    }
+
+    /// Append an element.
+    #[inline]
+    pub fn push(&mut self, t: T) {
+        self.0.push(t)
+    }
+
+    /// Return the total length.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.0.len()
     }
 }
 
+
+// *** Deserializing NonEmptyWithCursor.
+// This is way more work than it should be. We just want to *validate* the data after parsing,
+// while using the normal parser. It'd be nice to just pass off a derived Deserialize, but we have
+// no way to do that.
 #[derive(Deserialize)]
-struct SerializedNE<T> {
+struct FakeNEC<T> {
     cursor: usize,
     data: Vec<T>,
 }
-
-// Can't figure out how to use custom error types in serde deserialization
-
-// #[derive(Eq, PartialEq, Debug)]
-// pub enum DeserializeNonEmptyError {
-//     InvalidCursor(usize, usize),
-//     Empty,
-// }
-//
-// impl fmt::Display for DeserializeNonEmptyError {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         match self {
-//             &DeserializeNonEmptyError::InvalidCursor(cursor, size) => {
-//                 write!(f,
-//                        "Cursor of {} should not be larger than length {}",
-//                        cursor,
-//                        size)
-//             }
-//             &DeserializeNonEmptyError::Empty => write!(f, "data must have at least one element"),
-//         }
-//     }
-// }
 
 impl<T> Deserialize for NonEmptyWithCursor<T>
     where T: Deserialize
@@ -181,69 +206,62 @@ impl<T> Deserialize for NonEmptyWithCursor<T>
     fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
         where D: Deserializer
     {
-        let mut x: SerializedNE<T> = Deserialize::deserialize(deserializer)?;
+        let x: FakeNEC<T> = Deserialize::deserialize(deserializer)?;
         if x.data.len() == 0 {
-            return Err(serde::de::Error::invalid_length(0));
+            Err(serde::de::Error::invalid_length(0))
+        } else if x.cursor >= x.data.len() {
+            Err(serde::de::Error::invalid_value(&format!("Cursor of {} out of bounds for vec of \
+                                                          length {}",
+                                                         x.cursor,
+                                                         x.data.len())))
+        } else {
+            let res: NonEmptyWithCursor<T> = NonEmptyWithCursor {
+                cursor: x.cursor,
+                data: NonEmpty::from_vec(x.data).unwrap(),
+            };
+            Ok(res)
         }
-        let most: Vec<T> = x.data.drain(1..).collect();
-        if x.cursor > most.len() {
-            return Err(serde::de::Error::invalid_value(&format!("Cursor of {} should not be \
-                                                                larger than length {}",
-                                                                x.cursor,
-                                                                most.len() + 1)));
+    }
+}
+
+// *** Likewise for NonEmpty
+impl<T> Deserialize for NonEmpty<T>
+    where T: Deserialize
+{
+    fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
+        where D: Deserializer
+    {
+        let x: Vec<T> = Deserialize::deserialize(deserializer)?;
+        if x.len() == 0 {
+            Err(serde::de::Error::invalid_length(0))
+        } else {
+            Ok(NonEmpty(x))
         }
-        let res: NonEmptyWithCursor<T> = NonEmptyWithCursor {
-            cursor: x.cursor,
-            head: x.data.pop().unwrap(),
-            most: most,
-        };
-        Ok(res)
-
     }
 }
 
-/// An iterator of `&T` to the contents of a `NonEmptyWithCursor<T>`.
-pub struct NEIter<'a, T: 'a> {
-    necurs: &'a NonEmptyWithCursor<T>,
-    current: usize,
-}
 
-impl<'a, T> Iterator for NEIter<'a, T> {
-    type Item = &'a T;
-
-    #[inline]
-    fn next(&mut self) -> Option<&'a T> {
-        let r = self.necurs.get(self.current);
-        self.current += 1;
-        r
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        let l = self.necurs.len();
-        (l, Some(l))
-    }
-}
-
-/// An iterator of `&mut T` to the contents of a `NonEmptyWithCursor<T>`
-pub struct NEIterMut<'a, T: 'a> {
-    iter: std::iter::Chain<std::iter::Once<&'a mut T>, std::slice::IterMut<'a, T>>,
-}
-
-impl<'a, T> Iterator for NEIterMut<'a, T> {
-    type Item = &'a mut T;
-
-    #[inline]
-    fn next(&mut self) -> Option<&'a mut T> {
-        self.iter.next()
-    }
-    #[inline]
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        self.iter.size_hint()
-    }
+#[test]
+fn test_serialize_deserialize_nonempty() {
+    let ne: NonEmpty<i32> = NonEmpty::new_with_rest(5, vec![50, 55]);
+    assert_eq!(serde_json::to_string(&ne).unwrap(), "[5,50,55]");
+    let parsed: Result<NonEmpty<i32>, _> = serde_json::from_str("[5,50,55]");
+    assert_eq!(parsed.unwrap(), ne);
 }
 
 #[test]
-fn test_nonempty_set_cursor() {
+fn test_deserialize_invalid_nonempty() {
+    let parsed: Result<NonEmpty<i32>, _> = serde_json::from_str("[]");
+    match parsed {
+        Ok(x) => panic!("Somehow this parsed: {:?}", x),
+        Err(SJE::Error::Syntax(SJE::ErrorCode::InvalidLength(0), 0, 0)) => {}
+        _ => panic!("Unexpected error"),
+    }
+}
+
+
+#[test]
+fn test_set_cursor() {
     let mut ne: NonEmptyWithCursor<i32> = NonEmptyWithCursor::new(1);
     assert_eq!(ne.get_cursor(), 0);
     assert_eq!(ne.get_current(), &1);
@@ -266,10 +284,8 @@ fn test_nonempty_set_cursor() {
 }
 
 #[test]
-fn test_nonempty_serialize_deserialize() {
-    let mut ne: NonEmptyWithCursor<i32> = NonEmptyWithCursor::new(5);
-    ne.push(50);
-    ne.push(55);
+fn test_serialize_deserialize_with_cursor() {
+    let ne: NonEmptyWithCursor<i32> = NonEmptyWithCursor::new_with_rest(5, vec![50, 55]);
     assert_eq!(serde_json::to_string(&ne).unwrap(),
                "{\"cursor\":0,\"data\":[5,50,55]}");
     match serde_json::from_str("{\"cursor\":0,\"data\":[5,50,55]}") {
@@ -280,9 +296,9 @@ fn test_nonempty_serialize_deserialize() {
 
 #[test]
 fn test_deserialize_invalid_cursor() {
-    let res: Result<NonEmptyWithCursor<i32>, _> = serde_json::from_str("{\"cursor\":2,\"data\":\
+    let res: Result<NonEmptyWithCursor<i32>, _> = serde_json::from_str("{\"cursor\":1,\"data\":\
                                                                         [5]}");
-    let exmsg = "Cursor of 2 should not be larger than length 1";
+    let exmsg = "Cursor of 1 out of bounds for vec of length 1";
     match res {
         Ok(x) => panic!("Should not have parsed to {:?}", x),
         // TODO: position here is 0, 0 because our parser is dumb.
@@ -305,10 +321,17 @@ fn test_deserialize_invalid_empty() {
 
 #[test]
 fn test_iter() {
-    let mut ne: NonEmptyWithCursor<i32> = NonEmptyWithCursor::new(5);
-    ne.push(50);
-    ne.push(55);
-    ne.set_cursor(2); // the cursor does not affect iteration
+    let ne: NonEmpty<i32> = NonEmpty::new_with_rest(5, vec![50, 55]);
     let v: Vec<&i32> = ne.iter().collect();
     assert_eq!(v, vec![&5, &50, &55]);
+}
+
+
+#[cfg(test)]
+#[derive(Debug)]
+struct A(());
+
+#[test]
+fn test_non_clonable() {
+    NonEmpty::new(A(()));
 }
