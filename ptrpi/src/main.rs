bca1d1dfd3504af7eb5991c3af6af16616c14946
Christopher Armstrong <radix@twistedmatrix.com>
2017-01-14 19:15:32 -0600

    return JSON representation of the App :D :D :D

--- ptrpi/src/main.rs
+++ ptrpi/src/main.rs
@@ -1,15 +1,19 @@
 extern crate hyper;
 extern crate futures;
+extern crate serde_json;
 
 extern crate pandt;
 
 use std::env;
+use std::cell::RefCell;
+use std::cell::Ref;
 
 use hyper::{Get, StatusCode};
 use hyper::header::ContentType;
 use hyper::server::{Server, Service, Request, Response};
 
-struct PT;
+#[derive(Clone)]
+struct PT(RefCell<pandt::app::App>);
 
 impl Service for PT {
     type Request = Request;
@@ -20,23 +24,27 @@ impl Service for PT {
     fn call(&self, req: Request) -> Self::Future {
         ::futures::finished(match (req.method(), req.path()) {
             (&Get, Some("/")) => {
-                Response::new().with_header(ContentType::html()).with_body("Hello, world!")
+                Response::new()
+                    .with_header(ContentType::json())
+                    .with_body(serde_json::to_string(&*self.0.borrow()).unwrap())
             }
             _ => Response::new().with_status(StatusCode::NotFound),
         })
     }
 }
 
-
 fn main() {
     let addr = format!("0.0.0.0:{}",
                        env::args().nth(1).unwrap_or(String::from("1337")))
         .parse()
         .unwrap();
+    let mut app = pandt::app::App::new(pandt::game::Game::new());
+    let pt = PT(RefCell::new(app));
     let (listening, server) = Server::standalone(|tokio| {
+            let pt = pt.clone();
             Server::http(&addr, tokio)
                 ?
-                .handle(|| Ok(PT), tokio)
+                .handle(move || Ok(pt.clone()), tokio)
         })
         .unwrap();
     println!("Listening on http://{}", listening);
