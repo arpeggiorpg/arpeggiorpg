4cc69d798f48ed098c1add81c73fa5326ef8161f
Christopher Armstrong <radix@twistedmatrix.com>
2017-01-28 22:40:45 -0600

    Put pathfinding code in the public grid module

--- pandt/src/grid.rs
+++ pandt/src/grid.rs
@@ -1,5 +1,6 @@
 extern crate nalgebra as na;
 extern crate ncollide as nc;
+use odds::vec::VecExt;
 
 use self::na::{Isometry3, Vector3};
 use self::nc::shape::Cuboid;
@@ -33,11 +34,211 @@ pub fn creature_within_distance(c1: &Creature, c2: &Creature, d: Distance) -> bo
     point3_distance(c1.pos(), c2.pos()) <= d
 }
 
+
+// FOLLOWING COPIED FROM PATHFINDING CRATE
+// ***************************************
+use num_traits::Zero;
+use std::collections::{BinaryHeap, HashMap};
+use std::hash::Hash;
+use std::cmp::Ordering;
+
+struct InvCmpHolder<K, P> {
+    key: K,
+    payload: P,
+}
+
+impl<K: PartialEq, P> PartialEq for InvCmpHolder<K, P> {
+    fn eq(&self, other: &InvCmpHolder<K, P>) -> bool {
+        self.key.eq(&other.key)
+    }
+}
+
+impl<K: PartialEq, P> Eq for InvCmpHolder<K, P> {}
+
+impl<K: PartialOrd, P> PartialOrd for InvCmpHolder<K, P> {
+    fn partial_cmp(&self, other: &InvCmpHolder<K, P>) -> Option<Ordering> {
+        other.key.partial_cmp(&self.key)
+    }
+}
+
+impl<K: Ord, P> Ord for InvCmpHolder<K, P> {
+    fn cmp(&self, other: &InvCmpHolder<K, P>) -> Ordering {
+        other.key.cmp(&self.key)
+    }
+}
+
+/// Find a path through the given DAG.
+/// parents: a HashMap containing a DAG of nodes, decorated with the cost of those connections.
+/// start: The node to start at, but actually *this is the *END* node of the A* search!!!!
+fn reverse_path<N: Eq + Hash>(mut parents: HashMap<N, N>, start: N) -> Vec<N> {
+    let mut path = vec![start];
+    while let Some(parent) = parents.remove(path.last().unwrap()) {
+        path.push(parent);
+    }
+    path.into_iter().rev().collect()
+}
+
+pub fn astar<N, C, FN, IN, FH, FS>(start: &N,
+                                   neighbours: FN,
+                                   heuristic: FH,
+                                   success: FS)
+                                   -> Option<(Vec<N>, C)>
+    where N: Eq + Hash + Clone,
+          C: Zero + Ord + Copy,
+          FN: Fn(&N) -> IN,
+          IN: IntoIterator<Item = (N, C)>,
+          FH: Fn(&N) -> C,
+          FS: Fn(&N) -> bool
+{
+    let mut to_see = BinaryHeap::new();
+    to_see.push(InvCmpHolder {
+        key: heuristic(start),
+        payload: (Zero::zero(), start.clone()),
+    });
+    let mut parents: HashMap<N, (N, C)> = HashMap::new();
+    while let Some(InvCmpHolder { payload: (cost, node), .. }) = to_see.pop() {
+        if success(&node) {
+            let parents = parents.into_iter().map(|(n, (p, _))| (n, p)).collect();
+            return Some((reverse_path(parents, node), cost));
+        }
+        // We may have inserted a node several time into the binary heap if we found
+        // a better way to access it. Ensure that we are currently dealing with the
+        // best path and discard the others.
+        if let Some(&(_, c)) = parents.get(&node) {
+            if cost > c {
+                continue;
+            }
+        }
+        for (neighbour, move_cost) in neighbours(&node) {
+            let old_cost = parents.get(&neighbour).map(|&(_, c)| c);
+            let new_cost = cost + move_cost;
+            if neighbour != *start && old_cost.map_or(true, |c| new_cost < c) {
+                parents.insert(neighbour.clone(), (node.clone(), new_cost));
+                let new_predicted_cost = new_cost + heuristic(&neighbour);
+                to_see.push(InvCmpHolder {
+                    key: new_predicted_cost,
+                    payload: (new_cost, neighbour),
+                });
+            }
+        }
+    }
+    None
+}
+
+pub fn astar_multi<N, C, FN, IN, FH>(start: &N,
+                                     neighbours: FN,
+                                     heuristic: FH,
+                                     mut successes: Vec<Box<Fn(&N) -> bool>>)
+                                     -> Vec<(Vec<N>, C)>
+    where N: Eq + Hash + Clone,
+          C: Zero + Ord + Copy,
+          FN: Fn(&N) -> IN,
+          IN: IntoIterator<Item = (N, C)>,
+          FH: Fn(&N) -> C
+{
+    let mut to_see = BinaryHeap::new();
+    to_see.push(InvCmpHolder {
+        key: heuristic(start),
+        payload: (Zero::zero(), start.clone()),
+    });
+    let mut parents: HashMap<N, (N, C)> = HashMap::new();
+    let mut found_nodes = vec![];
+    while let Some(InvCmpHolder { payload: (cost, node), .. }) = to_see.pop() {
+        successes.retain_mut(|ref mut success_fn| {
+            let was_successful = success_fn(&node);
+            if was_successful {
+                found_nodes.push((node.clone(), cost));
+            }
+            !was_successful
+        });
+        if successes.len() == 0 {
+            break;
+        }
+        // We may have inserted a node several time into the binary heap if we found
+        // a better way to access it. Ensure that we are currently dealing with the
+        // best path and discard the others.
+        if let Some(&(_, c)) = parents.get(&node) {
+            if cost > c {
+                continue;
+            }
+        }
+        for (neighbour, move_cost) in neighbours(&node) {
+            let old_cost = parents.get(&neighbour).map(|&(_, c)| c);
+            let new_cost = cost + move_cost;
+            if neighbour != *start && old_cost.map_or(true, |c| new_cost < c) {
+                parents.insert(neighbour.clone(), (node.clone(), new_cost));
+                let new_predicted_cost = new_cost + heuristic(&neighbour);
+                to_see.push(InvCmpHolder {
+                    key: new_predicted_cost,
+                    payload: (new_cost, neighbour),
+                });
+            }
+        }
+    }
+
+    let mut results = vec![];
+    let parents: HashMap<N, N> = parents.into_iter().map(|(n, (p, _))| (n, p)).collect();
+    for (found_node, cost) in found_nodes {
+        results.push((reverse_path(parents.clone(), found_node), cost));
+    }
+    results
+}
+
+// PRECEDING COPIED FROM PATHFINDING CRATE
+// ***************************************
+
+fn point3_neighbors(terrain: &Map, pt: Point3) -> Vec<(Point3, u32)> {
+    let mut results = vec![];
+    for x in -1..2 {
+        for y in -1..2 {
+            if (x, y) == (0, 0) {
+                continue;
+            }
+            let neighbor = (pt.0 + x, pt.1 + y, pt.2);
+            if !terrain.contains(&neighbor) {
+                let cost = if x.abs() == y.abs() { 141 } else { 100 };
+                results.push((neighbor, cost));
+            }
+        }
+    }
+    results
+}
+
+pub fn get_all_accessible(start: Point3, terrain: &Map, speed: Distance) -> Vec<Point3> {
+    let meters = (speed.0 / 100) as i16;
+    let mut points_to_check = vec![];
+    for x in start.0 - meters..start.0 + meters + 1 {
+        for y in start.1 - meters..start.1 + meters + 1 {
+            let end_point = (x, y, 0);
+            if end_point == start {
+                continue;
+            }
+            points_to_check.push(end_point);
+        }
+    }
+    println!("Number of points to check: {:?}", points_to_check.len());
+    let mut success_fns: Vec<Box<Fn(&Point3) -> bool>> = vec![];
+    for pt in points_to_check {
+        success_fns.push(Box::new(move |n: &Point3| *n == pt.clone()));
+    }
+    let mut final_points = vec![];
+    for (path, cost) in astar_multi(&start,
+                                    |n| point3_neighbors(terrain, *n),
+                                    |n| point3_distance(start, *n).0,
+                                    success_fns) {
+        if Distance(cost) <= speed {
+            // FIXME: we should NOT be checking cost here, instead astar_multi should support
+            // max distance.
+            final_points.push(path.last().unwrap().clone())
+        }
+    }
+    final_points
+}
+
+
 #[cfg(test)]
 pub mod test {
-    use std::fmt::Debug;
     use grid::*;
-    use odds::vec::VecExt;
 
     #[test]
     fn test_biggest_distance() {
@@ -68,186 +269,6 @@ pub mod test {
         let pos1 = (0, 0, 0);
         let pos2 = (1, 1, 0);
         assert_eq!(point3_distance(pos1, pos2), Distance::new(2.0f32.sqrt()));
-    }
-
-    // PATHFINDING MESSING ABOUT
-
-    use std::collections::VecDeque;
-    use std::vec::IntoIter;
-    use std::collections::HashSet;
-
-
-    // FOLLOWING COPIED FROM PATHFINDING CRATE
-    // ***************************************
-    use num_traits::Zero;
-    use std::collections::{BinaryHeap, HashMap};
-    use std::hash::Hash;
-    use std::cmp::Ordering;
-
-    struct InvCmpHolder<K, P> {
-        key: K,
-        payload: P,
-    }
-
-    impl<K: PartialEq, P> PartialEq for InvCmpHolder<K, P> {
-        fn eq(&self, other: &InvCmpHolder<K, P>) -> bool {
-            self.key.eq(&other.key)
-        }
-    }
-
-    impl<K: PartialEq, P> Eq for InvCmpHolder<K, P> {}
-
-    impl<K: PartialOrd, P> PartialOrd for InvCmpHolder<K, P> {
-        fn partial_cmp(&self, other: &InvCmpHolder<K, P>) -> Option<Ordering> {
-            other.key.partial_cmp(&self.key)
-        }
-    }
-
-    impl<K: Ord, P> Ord for InvCmpHolder<K, P> {
-        fn cmp(&self, other: &InvCmpHolder<K, P>) -> Ordering {
-            other.key.cmp(&self.key)
-        }
-    }
-
-    /// Find a path through the given DAG.
-    /// parents: a HashMap containing a DAG of nodes, decorated with the cost of those connections.
-    /// start: The node to start at, but actually *this is the *END* node of the A* search!!!!
-    fn reverse_path<N: Eq + Hash>(mut parents: HashMap<N, N>, start: N) -> Vec<N> {
-        let mut path = vec![start];
-        while let Some(parent) = parents.remove(path.last().unwrap()) {
-            path.push(parent);
-        }
-        path.into_iter().rev().collect()
-    }
-
-    pub fn astar<N, C, FN, IN, FH, FS>(start: &N,
-                                       neighbours: FN,
-                                       heuristic: FH,
-                                       success: FS)
-                                       -> Option<(Vec<N>, C)>
-        where N: Eq + Hash + Clone + Debug,
-              C: Zero + Ord + Copy + Debug,
-              FN: Fn(&N) -> IN,
-              IN: IntoIterator<Item = (N, C)>,
-              FH: Fn(&N) -> C,
-              FS: Fn(&N) -> bool
-    {
-        let mut to_see = BinaryHeap::new();
-        to_see.push(InvCmpHolder {
-            key: heuristic(start),
-            payload: (Zero::zero(), start.clone()),
-        });
-        let mut parents: HashMap<N, (N, C)> = HashMap::new();
-        while let Some(InvCmpHolder { payload: (cost, node), .. }) = to_see.pop() {
-            if success(&node) {
-                let parents = parents.into_iter().map(|(n, (p, _))| (n, p)).collect();
-                return Some((reverse_path(parents, node), cost));
-            }
-            // We may have inserted a node several time into the binary heap if we found
-            // a better way to access it. Ensure that we are currently dealing with the
-            // best path and discard the others.
-            if let Some(&(_, c)) = parents.get(&node) {
-                if cost > c {
-                    continue;
-                }
-            }
-            for (neighbour, move_cost) in neighbours(&node) {
-                let old_cost = parents.get(&neighbour).map(|&(_, c)| c);
-                let new_cost = cost + move_cost;
-                if neighbour != *start && old_cost.map_or(true, |c| new_cost < c) {
-                    println!("[parent] neighbor={:?}, parent={:?}, cost={:?}",
-                             neighbour,
-                             node,
-                             new_cost);
-                    parents.insert(neighbour.clone(), (node.clone(), new_cost));
-                    let new_predicted_cost = new_cost + heuristic(&neighbour);
-                    to_see.push(InvCmpHolder {
-                        key: new_predicted_cost,
-                        payload: (new_cost, neighbour),
-                    });
-                }
-            }
-        }
-        None
-    }
-
-    pub fn astar_multi<N, C, FN, IN, FH>(start: &N,
-                                         neighbours: FN,
-                                         heuristic: FH,
-                                         mut successes: Vec<Box<Fn(&N) -> bool>>)
-                                         -> Vec<(Vec<N>, C)>
-        where N: Eq + Hash + Clone + Debug,
-              C: Zero + Ord + Copy + Debug,
-              FN: Fn(&N) -> IN,
-              IN: IntoIterator<Item = (N, C)>,
-              FH: Fn(&N) -> C
-    {
-        let mut to_see = BinaryHeap::new();
-        to_see.push(InvCmpHolder {
-            key: heuristic(start),
-            payload: (Zero::zero(), start.clone()),
-        });
-        let mut parents: HashMap<N, (N, C)> = HashMap::new();
-        let mut found_nodes = vec![];
-        while let Some(InvCmpHolder { payload: (cost, node), .. }) = to_see.pop() {
-            successes.retain_mut(|ref mut success_fn| {
-                let was_successful = success_fn(&node);
-                if was_successful {
-                    found_nodes.push((node.clone(), cost));
-                }
-                !was_successful
-            });
-            if successes.len() == 0 {
-                break;
-            }
-            // We may have inserted a node several time into the binary heap if we found
-            // a better way to access it. Ensure that we are currently dealing with the
-            // best path and discard the others.
-            if let Some(&(_, c)) = parents.get(&node) {
-                if cost > c {
-                    continue;
-                }
-            }
-            for (neighbour, move_cost) in neighbours(&node) {
-                let old_cost = parents.get(&neighbour).map(|&(_, c)| c);
-                let new_cost = cost + move_cost;
-                if neighbour != *start && old_cost.map_or(true, |c| new_cost < c) {
-                    parents.insert(neighbour.clone(), (node.clone(), new_cost));
-                    let new_predicted_cost = new_cost + heuristic(&neighbour);
-                    to_see.push(InvCmpHolder {
-                        key: new_predicted_cost,
-                        payload: (new_cost, neighbour),
-                    });
-                }
-            }
-        }
-
-        let mut results = vec![];
-        let parents: HashMap<N, N> = parents.into_iter().map(|(n, (p, _))| (n, p)).collect();
-        for (found_node, cost) in found_nodes {
-            results.push((reverse_path(parents.clone(), found_node), cost));
-        }
-        results
-    }
-
-    // PRECEDING COPIED FROM PATHFINDING CRATE
-    // ***************************************
-
-    fn point3_neighbors(terrain: &Map, pt: Point3) -> Vec<(Point3, u32)> {
-        let mut results = vec![];
-        for x in -1..2 {
-            for y in -1..2 {
-                if (x, y) == (0, 0) {
-                    continue;
-                }
-                let neighbor = (pt.0 + x, pt.1 + y, pt.2);
-                if !terrain.contains(&neighbor) {
-                    let cost = if x.abs() == y.abs() { 141 } else { 100 };
-                    results.push((neighbor, cost));
-                }
-            }
-        }
-        results
     }
 
     #[test]
@@ -305,37 +326,6 @@ pub mod test {
         let ex_path_negative = vec![(0, 0, 0), (-1, -1, 0)];
         assert_eq!(paths_and_costs,
                    [(ex_path_positive, 141), (ex_path_negative, 141)]);
-    }
-
-    fn get_all_accessible(start: Point3, terrain: &Map, speed: Distance) -> Vec<Point3> {
-        let meters = (speed.0 / 100) as i16;
-        let mut points_to_check = vec![];
-        for x in start.0 - meters..start.0 + meters + 1 {
-            for y in start.1 - meters..start.1 + meters + 1 {
-                let end_point = (x, y, 0);
-                if end_point == start {
-                    continue;
-                }
-                points_to_check.push(end_point);
-            }
-        }
-        println!("Number of points to check: {:?}", points_to_check.len());
-        let mut success_fns: Vec<Box<Fn(&Point3) -> bool>> = vec![];
-        for pt in points_to_check {
-            success_fns.push(Box::new(move |n: &Point3| *n == pt.clone()));
-        }
-        let mut final_points = vec![];
-        for (path, cost) in astar_multi(&start,
-                                        |n| point3_neighbors(terrain, *n),
-                                        |n| point3_distance(start, *n).0,
-                                        success_fns) {
-            if Distance(cost) <= speed {
-                // FIXME: we should NOT be checking cost here, instead astar_multi should support
-                // max distance.
-                final_points.push(path.last().unwrap().clone())
-            }
-        }
-        final_points
     }
 
     /// A map containing a "room" that only has one free space in the middle.
@@ -402,11 +392,10 @@ pub mod test {
         assert_eq!(pts, expected)
     }
 
-    // #[test]
-    // fn test_accessible_average_speed() {
-    //     let terrain = vec![];
-    //     let pts = get_all_accessible((0, 0, 0), &terrain, Distance(1000));
-    //     assert_eq!(pts.len(), 316);
-    // }
-
+    #[test]
+    fn test_accessible_average_speed() {
+        let terrain = vec![];
+        let pts = get_all_accessible((0, 0, 0), &terrain, Distance(1000));
+        assert_eq!(pts.len(), 316);
+    }
 }
