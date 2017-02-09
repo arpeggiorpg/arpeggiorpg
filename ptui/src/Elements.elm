ec10dc1d424a22681d97ac27463b5cedbeffc0d9
Christopher Armstrong <radix@twistedmatrix.com>
2017-02-09 10:35:41 -0600

    move Elm code into src subdir
    also delete some unused code (MouseEvents.elm, and a function from Elements.elm)

--- ptui/src/Elements.elm
+++ ptui/src/Elements.elm
@@ -12,12 +12,3 @@ hbox els = div [style [("display", "flex"), ("width", "100%")] ]
 vbox : List (Html a) -> Html a
 vbox els = div [style [("display", "flex"), ("flex-direction", "column"), ("width", "100%")]]
                (List.map (\el -> div [style [("flex-grow", "1")]] [el]) els)
-
--- An element whose *center* is positioned relative to its parents at specific x/y pixel offsets.
-centerPositionedBox : String -> String -> List (Attribute msg) -> List (Html msg) -> Html msg
-centerPositionedBox x y attrs content =
-  div [style [ ("position", "absolute")
-             , ("left", x)
-             , ("top", y)]]
-      [div (attrs ++ [style [("position", "relative"), ("margin-left", "-50%"), ("margin-top", "-50%")]])
-           content]
