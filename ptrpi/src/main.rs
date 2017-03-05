8f30522f3a2b925dcdd86709051b76966f87af93
Christopher Armstrong <radix@twistedmatrix.com>
2017-03-05 11:25:58 -0600

    make the RPI more safe
    this introduces the use of error-chain into the RPI, and removes all unwrap/expect calls in favor of using Result.
    
    This should drastically reduce the possibility of poisoning either of our locks.
    
    Error chain gives us the ability to very easily convert other errors (like GameError and serde_json Error) into a single RPIError type without anything other than `?`.

--- ptrpi/src/main.rs
+++ ptrpi/src/main.rs
@@ -3,6 +3,8 @@
 #![plugin(rocket_codegen)]
 
 extern crate bus;
+#[macro_use]
+extern crate error_chain;
 extern crate rocket;
 extern crate rocket_contrib;
 extern crate serde;
@@ -29,7 +31,25 @@ use cors::{CORS, PreflightCORS};
 use pandt::types::{App, RPIApp, AbilityID, CreatureID, GameCommand, GameError, Point3,
                    PotentialTarget};
 
-type PTResult<X> = Result<CORS<JSON<X>>, GameError>;
+
+error_chain! {
+  types { RPIError, RPIErrorKind, RPIResultExt; }
+
+  foreign_links {
+    GameError(GameError);
+    JSONError(serde_json::error::Error);
+  }
+
+  errors {
+    LockError(resource: String) {
+      description("Some locked resource is poisoned. The application probably needs restarted.")
+      display("The lock on {} is poisoned. The application probably needs restarted.", resource)
+    }
+  }
+}
+
+
+type PTResult<X> = Result<CORS<JSON<X>>, RPIError>;
 
 #[derive(Clone)]
 struct PT {
@@ -38,8 +58,12 @@ struct PT {
 }
 
 impl PT {
-  fn app(&self) -> MutexGuard<App> {
-    self.app.lock().expect("Couldn't acquire App!")
+  fn app(&self) -> Result<MutexGuard<App>, RPIError> {
+    self.app.lock().map_err(|_| RPIErrorKind::LockError("app".to_string()).into())
+  }
+
+  fn pollers(&self) -> Result<MutexGuard<bus::Bus<()>>, RPIError> {
+    self.pollers.lock().map_err(|_| RPIErrorKind::LockError("pollers".to_string()).into())
   }
 }
 
@@ -49,58 +73,58 @@ fn options_handler<'a>() -> PreflightCORS {
 }
 
 #[get("/")]
-fn get_app(pt: State<PT>) -> CORS<String> {
-  let app = pt.app();
-  let result = serde_json::to_string(&RPIApp(&*app)).unwrap();
-  CORS::any(result)
+fn get_app(pt: State<PT>) -> Result<CORS<String>, RPIError> {
+  let app = pt.app()?;
+  let result = serde_json::to_string(&RPIApp(&*app))?;
+  Ok(CORS::any(result))
 }
 
 /// If the client is polling with a non-current app "version", then immediately return the current
 /// App. Otherwise, wait 30 seconds for any new changes.
 #[get("/poll/<snapshot_len>/<log_len>")]
-fn poll_app(pt: State<PT>, snapshot_len: usize, log_len: usize) -> CORS<String> {
+fn poll_app(pt: State<PT>, snapshot_len: usize, log_len: usize) -> Result<CORS<String>, RPIError> {
   {
-    let app = pt.app();
+    let app = pt.app()?;
     if app.snapshots.len() != snapshot_len ||
        app.snapshots.back().map(|&(_, ref ls)| ls.len()).unwrap_or(0) != log_len {
-      let result = serde_json::to_string(&RPIApp(&*app)).unwrap();
-      return CORS::any(result);
+      let result = serde_json::to_string(&RPIApp(&*app))?;
+      return Ok(CORS::any(result));
     }
   }
 
-  let mut reader = pt.pollers.lock().unwrap().add_rx();
+  let mut reader = pt.pollers()?.add_rx();
   // this will either return a timeout or (); in any case we'll just return the App to the client.
   let _ = reader.recv_timeout(time::Duration::from_secs(30));
   get_app(pt)
 }
 
 #[post("/", format = "application/json", data = "<command>")]
-fn post_app(command: JSON<GameCommand>, pt: State<PT>) -> CORS<String> {
+fn post_app(command: JSON<GameCommand>, pt: State<PT>) -> Result<CORS<String>, RPIError> {
   let json = {
-    let mut app = pt.app();
+    let mut app = pt.app()?;
     let result = app.perform_unchecked(command.0);
     serde_json::to_string(&result)
   };
-  pt.pollers.lock().unwrap().broadcast(());
-  CORS::any(json.expect("Couldn't serialize App response"))
+  pt.pollers()?.broadcast(());
+  Ok(CORS::any(json?))
 }
 
 #[get("/combat_movement_options")]
 fn combat_movement_options(pt: State<PT>) -> PTResult<Vec<Point3>> {
-  let app = pt.app();
+  let app = pt.app()?;
   Ok(CORS::any(JSON(app.get_combat_movement_options()?)))
 }
 
 #[get("/movement_options/<cid>")]
 fn movement_options(pt: State<PT>, cid: &str) -> PTResult<Vec<Point3>> {
-  let app = pt.app();
+  let app = pt.app()?;
   let cid = CreatureID::new(cid)?;
   Ok(CORS::any(JSON(app.get_movement_options(cid)?)))
 }
 
 #[get("/target_options/<cid>/<abid>")]
 fn target_options(pt: State<PT>, cid: &str, abid: &str) -> PTResult<Vec<PotentialTarget>> {
-  let app = pt.app();
+  let app = pt.app()?;
   let cid = CreatureID::new(cid)?;
   let abid = AbilityID::new(abid)?;
   Ok(CORS::any(JSON(app.get_target_options(cid, abid)?)))
