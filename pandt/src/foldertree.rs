a81f672f842585fc9ae76e797d61059559c97ae8
Christopher Armstrong <radix@twistedmatrix.com>
2017-03-16 11:12:08 -0500

    Get foldertree actually building all the way

--- pandt/src/foldertree.rs
+++ pandt/src/foldertree.rs
@@ -11,16 +11,50 @@ pub struct FolderTree<T> {
   pub tree: LabeledTree<T>,
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize)]
+pub struct FolderPath(Vec<String>);
+
+impl FolderPath {
+  pub fn from_vec(segs: Vec<String>) -> FolderPath {
+    FolderPath(segs)
+  }
+}
+
 impl<T> FolderTree<T> {
-  pub fn new(rootNode: T) -> FolderTree<T> {
-    FolderTree { tree: RoseTree::new(("".to_string(), rootNode)).0 }
+  pub fn new(root_node: T) -> FolderTree<T> {
+    FolderTree { tree: RoseTree::new(("".to_string(), root_node)).0 }
+  }
+
+  pub fn get(&self, path: FolderPath) -> Option<&T> {
+    if path.0.len() == 0 {
+      return None;
+    }
+    let mut cur_idx = NodeIndex::new(ROOT);
+
+    fn find_child<A>(tree: &LabeledTree<A>, idx: NodeIndex, name: String) -> Option<NodeIndex> {
+      let children_indices = tree.children(idx);
+      for cidx in children_indices {
+        let child = &tree[cidx];
+        if child.0 == name {
+          return Some(cidx);
+        }
+      }
+      None
+    }
+    for seg in path.0 {
+      match find_child(&self.tree, cur_idx, seg) {
+        Some(cidx) => cur_idx = cidx,
+        None => return None
+      }
+    }
+    Some(&self.tree[cur_idx].1)
   }
 }
 
 impl<T: Eq> Eq for FolderTree<T> {}
 impl<T: PartialEq> PartialEq for FolderTree<T> {
   fn eq(&self, other: &FolderTree<T>) -> bool {
-    true
+    panic!("Implement equality")
   }
 }
 
@@ -61,15 +95,13 @@ impl<'a, T: Serialize> Serialize for ChildrenSerializer<'a, T> {
     let children_indices: Vec<NodeIndex> = self.tree.children(self.index).collect();
     let mut map = serializer.serialize_map(Some(children_indices.len()))?;
     for idx in children_indices {
-      let (name, node) = self.tree[idx];
-      map.serialize_key(&name)?;
+      let &(ref name, ref node) = &self.tree[idx];
+      let children_serializer = ChildrenSerializer { tree: &self.tree, index: idx};
       let helper = SerializerHelper {
-        data: &node,
-        children: ChildrenSerializer {
-          tree: &self.tree,
-          index: idx,
-        },
+        data: node,
+        children: children_serializer,
       };
+      map.serialize_key(&name)?;
       map.serialize_value(&helper)?;
     }
     map.end()
@@ -81,9 +113,9 @@ impl<T: Serialize> Serialize for FolderTree<T> {
     where S: Serializer
   {
     let root_idx = NodeIndex::new(ROOT);
-    let (_, root) = self.tree[root_idx];
+    let &(_, ref root) = &self.tree[root_idx];
     let helper = SerializerHelper {
-      data: &root,
+      data: root,
       children: ChildrenSerializer {
         tree: &self.tree,
         index: root_idx,
@@ -93,8 +125,7 @@ impl<T: Serialize> Serialize for FolderTree<T> {
   }
 }
 
-mod test {
-}
+mod test {}
 
 // impl<T: de::Deserialize> de::Deserialize for FolderTree<T> {
 //   fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: de::Deserializer {}
