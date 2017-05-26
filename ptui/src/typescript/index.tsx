9c6f73aa1614e6f3e9486b9e3106803ffc031dd2
Christopher Armstrong <radix@twistedmatrix.com>
2017-05-26 10:34:44 -0500

    delete Components.js and move its code into index.tsx

--- ptui/src/typescript/index.tsx
+++ ptui/src/typescript/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./Hello";
 
-export function renderHello(id: string) {
+function renderHello(id: string) {
   console.log("[renderHello]", id);
   ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
@@ -11,14 +11,29 @@ export function renderHello(id: string) {
   );
 }
 
-export function unloadHello(id: string) {
+function unloadHello(id: string) {
   let el =  document.getElementById(id);
   if (el !== null) {
     ReactDOM.unmountComponentAtNode(el);
   }
 }
 
+function afterView(f: any) {
+  return function(this: any) {
+    let args = arguments;
+    let self = this;
+    window.requestAnimationFrame(function(_) {
+        f.apply(self, args);
+    });
+  }
+}
+
+function PT_initializeComponents(app: any) {
+  app.ports.renderHello.subscribe(afterView(renderHello));
+  app.ports.unloadHello.subscribe(afterView(unloadHello));
+}
+
 // I can't figure out any other way to export these functions such that they can be called from plain
 // old javascript callers
-(window as any).components =
-  { renderHello: renderHello, unloadHello: unloadHello};
+
+(window as any).PT_initializeComponents = PT_initializeComponents;
