2386251d80f02f0799b03530cf18651560358558
Christopher Armstrong <radix@twistedmatrix.com>
2017-05-26 19:32:24 -0500

    make TextInput auto-focus and auto-select

--- ptui/src/typescript/TextInput.tsx
+++ ptui/src/typescript/TextInput.tsx
@@ -8,26 +8,39 @@ interface TextInputProps {
   onCancel: (input: string) => void;
 }
 
-class TextInput extends React.Component<TextInputProps, {value: string}> {
+class TextInput extends React.Component<TextInputProps, { value: string }> {
+
+  private input: HTMLInputElement;
 
   constructor(props: TextInputProps) {
     super(props);
     this.state = { value: props.defaultValue };
+  }
+  cancel(): void {
+    this.props.onCancel(this.state.value);
   }
 
   handleKeyDown(this: TextInput, event: React.KeyboardEvent<HTMLInputElement>): void {
     if (event.keyCode == 13) {
       this.props.onSubmit(this.state.value);
     } else if (event.keyCode == 27) {
-      this.props.onCancel(this.state.value);
+      this.cancel();
     }
+  }
+
+  componentDidMount() {
+    this.input.select();
   }
 
   render() {
     return <input type="text"
-                  value={this.state.value}
-                  onChange={e => this.setState({value: e.target.value})}
-                  onKeyDown={ e => this.handleKeyDown(e)}></input>;
+      autoFocus
+      ref={(input) => this.input = input}
+      value={this.state.value}
+      onChange={e => this.setState({ value: e.target.value })}
+      onKeyDown={e => this.handleKeyDown(e)}
+      onBlur={e => this.cancel()}
+      ></input>;
   }
 }
 
@@ -39,7 +52,7 @@ export function renderTextInput(app: any, [id, defaultValue, styles]: [string, s
     app.ports.textInputCancel.send([id, content]);
   };
   ReactDOM.render(
-    <TextInput defaultValue={defaultValue} styles={styles} onSubmit={onSubmit} onCancel={onCancel}/>,
+    <TextInput defaultValue={defaultValue} styles={styles} onSubmit={onSubmit} onCancel={onCancel} />,
     document.getElementById(id)
   );
 }
