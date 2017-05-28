88c645099b76a0a740be2534572c5be45f0f650e
Christopher Armstrong <radix@twistedmatrix.com>
2017-05-27 23:46:04 -0500

    working through implementing the gamelog types & decoders

--- ptui/src/typescript/PTTypes.ts
+++ ptui/src/typescript/PTTypes.ts
@@ -5,6 +5,7 @@ import * as Lodash from 'lodash';
 type CreatureID = string;
 type SceneID = string;
 type AttrID = string;
+type MapID = string;
 
 
 export interface App {
@@ -15,12 +16,6 @@ export type AppSnapshots = Array<{ snapshot: GameSnapshot, logs: Array<GameLog> 
 
 export interface GameSnapshot { };
 
-interface AttributeCheck {
-  reliable: boolean;
-  attr: AttrID;
-  target: SkillLevel;
-}
-
 export type GameLog =
   | {
     t: "AttributeCheckResult";
@@ -30,11 +25,58 @@ export type GameLog =
     success: boolean;
   }
   | { t: "CreateFolder"; path: string }
+  | { t: "RenameFolder"; path: string; newName: string }
+  | { t: "DeleteFolder"; path: string }
+  | { t: "MoveFolderItem"; path: string; item: FolderItemID; newPath: string }
+  | { t: "CreateNote"; path: string; note: Note }
+  | { t: "EditNote"; path: string; name: string; newNote: Note }
+  | { t: "DeleteNote"; path: string; name: string }
   | { t: "StartCombat"; scene: SceneID; creatures: Array<{ cid: CreatureID; init: number }> }
   | { t: "StopCombat" }
 
+
+export type FolderItemID =
+  | { t: "SceneID"; id: SceneID }
+  | { t: "MapID"; id: MapID }
+  | { t: "CreatureID"; id: CreatureID }
+  | { t: "NoteID"; id: string }
+  | { t: "SubfolderID"; id: string }
+
+interface AttributeCheck {
+  reliable: boolean;
+  attr: AttrID;
+  target: SkillLevel;
+}
+
 type SkillLevel = "Inept" | "Unskilled" | "Skilled" | "Expert" | "Supernatural"
 let SkillLevel_values: Array<SkillLevel> = ["Inept", "Unskilled", "Skilled", "Expert", "Supernatural"];
+
+interface Note {
+  name: string,
+  content: string,
+}
+
+// CreateScene(FolderPath, Scene),
+// EditScene(Scene),
+// DeleteScene(SceneID),
+// CreateMap(FolderPath, Map),
+// EditMap(Map),
+// DeleteMap(MapID),
+// CombatLog(CombatLog),
+// /// A creature log wrapped in a game log.
+// CreatureLog(CreatureID, CreatureLog),
+// SetCreaturePos(SceneID, CreatureID, Point3),
+// PathCreature(SceneID, CreatureID, Vec<Point3>),
+// StartCombat(SceneID, Vec<(CreatureID, i16)>),
+// StopCombat,
+// CreateCreature(FolderPath, Creature),
+// EditCreature(Creature),
+// DeleteCreature(CreatureID),
+// AddCreatureToCombat(CreatureID, i16),
+// RemoveCreatureFromCombat(CreatureID),
+// /// Indexes into snapshots and logs.
+// Rollback(usize, usize),
+
 
 /// Decoders
 
@@ -45,24 +87,54 @@ const decodeAttributeCheck: Decoder<AttributeCheck> =
   JD.object(["reliable", JD.boolean()], ["attr", JD.string()], ["target", decodeSkillLevel],
     (reliable, attr, target) => ({ reliable, attr, target }))
 
+function _mkFolderItem(t: string): Decoder<FolderItemID> {
+  return JD.map((id) => ({ t, id } as FolderItemID), JD.string());
+}
+const decodeFolderItemID: Decoder<FolderItemID> =
+  sum<FolderItemID>("FolderItemID", {}, {
+    "SceneID": _mkFolderItem("SceneID"),
+    "MapID": _mkFolderItem("MapID"),
+    "CreatureID": _mkFolderItem("CreatureID"),
+    "NoteID": _mkFolderItem("NoteID"),
+    "SubfolderID": _mkFolderItem("SubfolderID"),
+  });
+
+const decodeNote: Decoder<Note> =
+  JD.object(
+    ["name", JD.string()],
+    ["content", JD.string()],
+    (name, content) => ({ name, content })
+  );
+
+
 export const decodeGameLog: Decoder<GameLog> =
   sum<GameLog>("GameLog", {
     "StopCombat": { t: "StopCombat" }
   }, {
       "StartCombat": JD.map(
-        ([scene, creatures]): GameLog => ({
-          t: "StartCombat",
-          scene: scene,
-          creatures: creatures,
-        }),
-        JD.tuple(JD.string(),
-          JD.array(JD.map(([cid, init]) => ({ cid: cid, init: init }),
-            JD.tuple(JD.string(), JD.number())
-          )
-          ))),
-      "CreateFolder": JD.map((p: string): GameLog => ({ t: "CreateFolder", path: p }), JD.string()),
-      "AttributeCheckResult":
-      JD.map(
+        ([scene, creatures]): GameLog => ({ t: "StartCombat", scene, creatures }),
+        JD.tuple(
+          JD.string(),
+          JD.array(JD.map(([cid, init]) => ({ cid, init }), JD.tuple(JD.string(), JD.number())))
+        )),
+      "CreateFolder": JD.map((p): GameLog => ({ t: "CreateFolder", path: p }), JD.string()),
+      "RenameFolder": JD.map(
+        ([path, newName]): GameLog => ({ t: "RenameFolder", path, newName }),
+        JD.tuple(JD.string(), JD.string())),
+      "DeleteFolder": JD.map((path): GameLog => ({ t: "DeleteFolder", path }), JD.string()),
+      "MoveFolderItem": JD.map(
+        ([path, item, newPath]): GameLog => ({ t: "MoveFolderItem", path, item, newPath }),
+        JD.tuple(JD.string(), decodeFolderItemID, JD.string())),
+      "CreateNote": JD.map(
+        ([path, note]): GameLog => ({ t: "CreateNote", path, note }),
+        JD.tuple(JD.string(), decodeNote)),
+      "EditNote": JD.map(
+        ([path, name, newNote]): GameLog => ({ t: "EditNote", path, name, newNote }),
+        JD.tuple(JD.string(), JD.string(), decodeNote)),
+      "DeleteNote": JD.map(
+        ([path, name]): GameLog => ({ t: "DeleteNote", path, name }),
+        JD.tuple(JD.string(), JD.string())),
+      "AttributeCheckResult": JD.map(
         ([cid, check, actual, success]): GameLog =>
           ({ t: "AttributeCheckResult", cid, check, actual, success }),
         JD.tuple(JD.string(), decodeAttributeCheck, JD.number(), JD.boolean())
