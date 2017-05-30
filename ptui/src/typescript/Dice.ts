1c5319b98b42e9642ed25694912224df55f608be
Christopher Armstrong <radix@twistedmatrix.com>
2017-05-30 15:37:16 -0500

    a dice parser! it works!

--- ptui/src/typescript/Dice.ts
+++ ptui/src/typescript/Dice.ts
@@ -1,8 +1,40 @@
 import * as P from 'Parsimmon';
 import * as T from './PTTypes';
 
-const LNumber = P.regexp(/[0-9]+/).map(Number).desc('number');
+function spaced<X>(parser: P.Parser<X>): P.Parser<X> {
+  return P.optWhitespace.then(parser).skip(P.optWhitespace);
+}
+
+const dice = P.lazy(function () { return _dice });
+
+const num =
+  spaced(
+    P.digits
+      .map(Number)
+      .desc('number'));
+const flat =
+  spaced(
+    num
+      .map((n) => ({ t: 'Flat', val: n }))
+      .desc('Flat'));
+const expr =
+  spaced(
+    P.seq(spaced(num).skip(spaced(P.string('d'))), spaced(num))
+      .map(([num, size]): T.Dice => ({ t: "Expr", num, size }))
+      .desc('Expr'));
+const plus: P.Parser<T.Dice> =
+  spaced(
+    P.seq(spaced(expr).skip(spaced(P.string('+'))), spaced(dice))
+      .map(([left, right]): T.Dice => ({ t: "Plus", left, right }))
+      .desc('Plus'));
+const bestof: P.Parser<T.Dice> =
+  spaced(
+    P.seq(spaced(P.string("BestOf")), spaced(P.string("(")), spaced(num), spaced(P.string(",")), spaced(dice), spaced(P.string(")")))
+      .map(([_, _2, num, _3, dice]): T.Dice => ({ t: "BestOf", num, dice }))
+      .desc("BestOf"));
+
+const _dice = P.alt(bestof, plus, expr, flat);
 
 export function parse(input: string): T.Dice {
-  return {t: 'Flat', val: LNumber.tryParse(input)}
+  return dice.tryParse(input);
 }
--- ptui/src/typescript/spec/DiceSpec.ts
+++ ptui/src/typescript/spec/DiceSpec.ts
@@ -1,11 +1,44 @@
 import * as D from '../Dice';
 
-let describe: any;
-let it: any;
-let expect: any;
+// let describe: any;
+// let it: any;
+// let expect: any;
 
 describe("dice parsing", function () {
   it("parses a number", function () {
-    expect(D.parse("5")).toEqual({ t: 'Flat', val: 5 })
-  })
+    expect(D.parse("5"))
+      .toEqual({ t: 'Flat', val: 5 });
+  });
+  it("parses an expr", function () {
+    expect(D.parse("1d20"))
+      .toEqual({ t: "Expr", num: 1, size: 20 });
+  });
+  it("parses a plus", function () {
+    expect(D.parse("1d20+5"))
+      .toEqual({ t: "Plus", left: { t: "Expr", num: 1, size: 20 }, right: { t: "Flat", val: 5 } });
+  });
+  it("parses a plus of two expressions", function () {
+    expect(D.parse("1d20+5d8"))
+      .toEqual({
+        t: "Plus",
+        left: { t: "Expr", num: 1, size: 20 },
+        right: { t: "Expr", num: 5, size: 8 }
+      });
+  });
+  // it("parses a plus with the flat on the left", function () {
+  //   expect(D.parse("1+1d20"))
+  //     .toEqual({ t: "Plus", left: { t: "Flat", val: 1 }, right: { t: "Expr", num: 1, size: 20 } });
+  // });
+  it("parses a BestOf", function () {
+    expect(D.parse("BestOf(5, 1d20)"))
+      .toEqual({ t: "BestOf", num: 5, dice: { t: "Expr", num: 1, size: 20 } });
+  });
+
+  it("ignores whitespace", function() {
+    expect(D.parse(" BestOf ( 5 ,1 d 20 ) "))
+      .toEqual({ t: "BestOf", num: 5, dice: { t: "Expr", num: 1, size: 20 } });
+
+  });
+
+
 });
