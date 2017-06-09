365b0bfd2bc12705c93088ca5a04f7aaed8994b3
Christopher Armstrong <radix@twistedmatrix.com>
2017-06-08 23:33:31 -0500

    something resembling a creature card in the React sidebar!

--- ptui/src/typescript/PlayerView.tsx
+++ ptui/src/typescript/PlayerView.tsx
@@ -31,15 +31,74 @@ class PlayerCreatures extends React.Component<
   { playerID: T.PlayerID; app: T.App; },
   undefined> {
 
-  creatureCard(creature: T.Creature): JSX.Element {
-    return <div>{creature.name}</div>;
+  creatureCard(cid: T.CreatureID): JSX.Element {
+    return <CreatureCard key={cid} app={this.props.app} creature_id={cid} />;
   }
 
   render(): JSX.Element {
     let cids = this.props.app.players[this.props.playerID].creatures;
-    let creatures = T.getCreatures(this.props.app, cids);
     return <div>Player creatures!
-      {creatures.map(this.creatureCard)}
+      {cids.map(this.creatureCard.bind(this))}
     </div>
   }
+}
+
+class CreatureCard extends React.Component<{ creature_id: T.CreatureID; app: T.App }, undefined> {
+  render(): JSX.Element {
+    let creature = T.getCreature(this.props.app, this.props.creature_id);
+    if (!creature) {
+      return <div>Creature {this.props.creature_id} not found</div>;
+    } else {
+
+      return <div
+        style={{
+          display: "flex", flexDirection: "column",
+          width: "300px",
+          borderRadius: "10px", border: "1px solid black",
+          padding: "3px"
+        }}>
+        <div><strong>{creature.name}</strong> {classIcon(creature)}</div>
+        <div style={{ display: "flex" }}>
+          {creatureIcon(this.props.app, creature)}
+          {/*, hbox (List.map conditionIcon (Dict.values creature.conditions))
+      ] ++ extras*/}
+        </div>
+      </div>;
+    }
+  }
+}
+
+function classIcon(creature: T.Creature): string {
+  switch (creature.class_) {
+    case "cleric": return "💉";
+    case "rogue": return "🗡️";
+    case "ranger": return "🏹";
+    case "creature": return "🏃";
+    case "baddie": return "👹";
+    default: return ""
+  }
+}
+
+function creatureIcon(app: T.App, creature: T.Creature): JSX.Element | null {
+  if (creature.portrait_url !== "") {
+    return <img src={creature.portrait_url} style={{ width: "50px", height: "50px", borderRadius: "10px", border: "solid 1px black" }} />
+  } else {
+    return null;
+  }
+  // let class_ = app.current_game.classes[creature.class_];
+  //   let creatureColor =
+  //         case Dict.get creature.class app.current_game.classes of
+  //           Just class -> class.color
+  //           Nothing -> "red"
+  //   in
+  //     if creature.portrait_url /= ""
+  //     then
+  //       img (stdStyle ++ [src creature.portrait_url
+  //           , s [S.width (S.px 50), S.height (S.px 50), S.borderRadius (S.px 10), plainBorder]])
+  //           []
+  //     else
+  //       sdiv [s [ S.width (S.px 50), S.height (S.px 50), S.borderRadius (S.px 10), plainBorder]
+  //           , style [("background-color", creatureColor)] ]
+  //           [text creature.name]
+
 }
