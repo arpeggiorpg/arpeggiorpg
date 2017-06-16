353830c10285b8723fabab4f92d61ad28d2685ce
Christopher Armstrong <radix@twistedmatrix.com>
2017-06-16 16:12:48 -0500

    hammerjs integration in ReactPlayer!

--- ptui/src/typescript/Grid.tsx
+++ ptui/src/typescript/Grid.tsx
@@ -1,3 +1,4 @@
+import * as Hammer from 'hammerjs';
 import * as LD from "lodash";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
@@ -101,7 +102,10 @@ interface GridSvgProps {
   map: T.Map;
   creatures: Array<MapCreature>;
 }
-interface GridSvgState { spz_element: SvgPanZoom.Instance | undefined; }
+interface GridSvgState {
+  spz_element: SvgPanZoom.Instance | undefined;
+  hammer?: HammerManager;
+}
 class GridSvgComp extends React.Component<GridSvgProps & M.ReduxProps, GridSvgState> {
 
   constructor(props: GridSvgProps & M.ReduxProps) {
@@ -109,13 +113,81 @@ class GridSvgComp extends React.Component<GridSvgProps & M.ReduxProps, GridSvgSt
     this.state = { spz_element: undefined };
   }
 
+  panzoomEvents() {
+    const self = this;
+    // init: (options: CustomEventOptions) => void;
+    // haltEventListeners: string[];
+    // destroy: Function;
+    return {
+      haltEventListeners: ['touchstart', 'touchend', 'touchmove', 'touchleave', 'touchcancel'],
+      init: (options: SvgPanZoom.CustomEventOptions) => {
+        let initialScale = 1;
+        let pannedX = 0;
+        let pannedY = 0;
+        // Init Hammer
+        // Listen only for pointer and touch events
+        const hammer = new Hammer(options.svgElement, {
+          inputClass: (Hammer as any).SUPPORT_POINTER_EVENTS
+            ? Hammer.PointerEventInput : Hammer.TouchInput,
+        });
+        self.setState({hammer});
+        // Enable pinch
+        hammer.get('pinch').set({ enable: true });
+        // Handle pan
+        hammer.on('panstart panmove', ev => {
+          // On pan start reset panned variables
+          if (ev.type === 'panstart') {
+            pannedX = 0;
+            pannedY = 0;
+          }
+          // Pan only the difference
+          options.instance.panBy({ x: ev.deltaX - pannedX, y: ev.deltaY - pannedY });
+          pannedX = ev.deltaX;
+          pannedY = ev.deltaY;
+        });
+        // Handle pinch
+        hammer.on('pinchstart pinchmove', ev => {
+          // On pinch start remember initial zoom
+          if (ev.type === 'pinchstart') {
+            initialScale = options.instance.getZoom();
+            options.instance.zoom(initialScale * ev.scale);
+          }
+          options.instance.zoom(initialScale * ev.scale);
+        });
+        // Prevent moving the page on some devices when panning over SVG
+        options.svgElement.addEventListener('touchmove', e => e.preventDefault());
+
+        // // See [Note: Panning/Clicking State Management]
+        // options.svgElement.addEventListener('mousedown', function () {
+        //   state.isMouseDown = true;
+        // });
+        // options.svgElement.addEventListener('mousemove', function () {
+        //   if (state.isMouseDown) {
+        //     app.ports.panning.send(true);
+        //   }
+        // });
+        // options.svgElement.addEventListener('touchend', function (e) {
+        //   app.ports.panning.send(false);
+        // });
+        // options.svgElement.addEventListener('mouseup', function () {
+        //   state.isMouseDown = false;
+        // })
+        // options.svgElement.addEventListener('click', function () {
+        //   app.ports.panning.send(false);
+        //   state.isMouseDown = false;
+        // });
+      },
+      destroy: () => { if (self.state.hammer) { self.state.hammer.destroy(); } },
+    };
+  }
+
+
   componentDidMount() {
     const pz = svgPanZoom("#pt-grid", {
       dblClickZoomEnabled: false,
       center: true,
       fit: true,
-      // TODO: Hammer.js integration
-      // , customEventsHandler: eventsHandler
+      customEventsHandler: this.panzoomEvents(),
       zoomScaleSensitivity: 0.5,
     });
     this.setState({ spz_element: pz });
