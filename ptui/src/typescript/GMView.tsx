e6f0ae96801babe58bbb15086b9c24ff963417e0
Christopher Armstrong <radix@twistedmatrix.com>
2017-06-19 09:43:25 -0500

    update the History view to be usable in the new react-based GM view

--- ptui/src/typescript/GMView.tsx
+++ ptui/src/typescript/GMView.tsx
@@ -3,7 +3,9 @@ import * as React from 'react';
 import * as Redux from 'redux';
 
 import * as CV from './CommonView';
+import * as History from './History';
 import * as M from './Model';
+import * as Players from './Players';
 import * as T from './PTTypes';
 
 export class GMMain extends React.Component<{}, undefined> {
@@ -12,8 +14,8 @@ export class GMMain extends React.Component<{}, undefined> {
     const tabs = [
       <CV.Tab key="Campaign" name="Campaign"><div>Campaign!</div></CV.Tab>,
       <CV.Tab key="Combat" name="Combat"><CV.Combat /></CV.Tab>,
-      <CV.Tab key="Players" name="Players"><div>Players go here!</div></CV.Tab>,
-      <CV.Tab key="History" name="History"><div>History!</div></CV.Tab>,
+      <CV.Tab key="Players" name="Players"><div>Players!</div></CV.Tab>,
+      <CV.Tab key="History" name="History"><History.History /></CV.Tab>,
       <CV.Tab key="SavedGames" name="Saved Games"><div>Saved Games!</div></CV.Tab>,
     ];
     return <CV.TheLayout map={map} tabs={tabs} under={<div>Hello!</div>} />;
--- ptui/src/typescript/History.tsx
+++ ptui/src/typescript/History.tsx
@@ -6,137 +6,136 @@ import * as M from './Model';
 import * as T from './PTTypes';
 
 export function renderHistory(app: any, [id, data]: [string, Array<Array<[any, Array<any>]>>]) {
-  const onRollback = (si: number, li: number) => app.ports.historyRollback.send([si, li]);
-  ReactDOM.render(
-    <History data={data} onRollback={onRollback} />,
-    document.getElementById(id)
-  );
+  // const onRollback = (si: number, li: number) => app.ports.historyRollback.send([si, li]);
+  // ReactDOM.render(
+  //   <History data={data} onRollback={onRollback} />,
+  //   document.getElementById(id)
+  // );
 }
 
-class History extends React.Component<
-  { data: any, onRollback: (snapshot_index: number, log_index: number) => void },
-  any> {
-  render(): JSX.Element {
-    const app = T.decodeApp.decodeAny(this.props.data);
-    console.log("rendering history. successfully decoded app.", app.snapshots);
-    return <Flexbox flexDirection="column">{
-      app.snapshots.map(
-        ({ snapshot, logs }, snapshot_index) =>
-          logs.map((log: T.GameLog, log_index) =>
-            <Flexbox key={snapshot_index.toString() + "-" + log_index.toString()}
-              justifyContent="space-between">
-              {this.gameLog(log)}
-              <button className="material-icons"
-                onClick={() => this.props.onRollback(snapshot_index, log_index)}
-              >history</button>
-            </Flexbox>)
-      )
-    }</Flexbox>;
-  }
+export const History = M.connectRedux(({ ptui, dispatch }: M.ReduxProps): JSX.Element => {
+  const app = ptui.app;
+  return <div>{
+    app.snapshots.map(
+      ({ snapshot, logs }, snapshot_index) =>
+        logs.map((log: T.GameLog, log_index) =>
+          <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}
+            key={snapshot_index.toString() + "-" + log_index.toString()}>
+            {gameLog(log)}
+            <button className="material-icons"
+              onClick={() =>
+                ptui.sendCommand(dispatch, { t: "Rollback", snapshot_index, log_index })}
+            >history</button>
+          </div>)
+    )
+  }</div>;
+});
 
-  gameLog(log: T.GameLog): JSX.Element | null {
-    switch (log.t) {
-      case "AttributeCheckResult":
-        return <Flexbox>
-          <div>Creature ID: {log.cid}</div>
-          <div>Success? {log.success.toString()}</div>
-        </Flexbox>;
-      case "CreateFolder":
-        return <Flexbox><div>Created Folder</div><div>{M.folderPathToString(log.path)}</div></Flexbox>;
-      case "RenameFolder":
-        return <Flexbox>Renamed Folder</Flexbox>
-      case "DeleteFolder":
-        return <Flexbox>Deleted folder {M.folderPathToString(log.path)}</Flexbox>
-      case "DeleteFolderItem":
-        return <Flexbox>Deleted folder item in {M.folderPathToString(log.path)}</Flexbox>
-      case "MoveFolderItem":
-        return <Flexbox>Moved folder item from {M.folderPathToString(log.path)}
-          to {M.folderPathToString(log.newPath)}</Flexbox>
-      case "CreateItem":
-        return <Flexbox>Created item {log.item.name} in {M.folderPathToString(log.path)}</Flexbox>
-      case "EditItem":
-        return <Flexbox>Edited item {log.item.name}</Flexbox>
-      case "CreateNote":
-        return <Flexbox>Created note {log.note.name}</Flexbox>
-      case "EditNote":
-        return <Flexbox>Edited note {log.name}</Flexbox>
-      case "DeleteNote":
-        return <Flexbox>Deleted note {log.name}</Flexbox>
-      case "CreateScene":
-        return <Flexbox>Created scene {log.scene.name}</Flexbox>
-      case "EditScene":
-        return <Flexbox>Edited scene {log.scene.name}</Flexbox>
-      case "DeleteScene":
-        return <Flexbox>Deleted a scene</Flexbox>
-      case "CreateMap":
-        return <Flexbox>Created a map {log.map.name}</Flexbox>
-      case "EditMap":
-        return <Flexbox>Edited a map {log.map.name}</Flexbox>
-      case "DeleteMap":
-        return <Flexbox>Deleted a map</Flexbox>
-      case "SetCreaturePos":
-        return <Flexbox>Set a creature position to {log.pos.toString()}</Flexbox>
-      case "PathCreature":
-        const last = log.path.length > 0
-          ? log.path[log.path.length - 1].toString()
-          : "Nowhere";
-        return <Flexbox>Creature followed a path to {last}</Flexbox>
-      case "CreateCreature":
-        return <Flexbox>Created a creature {log.creature.name}</Flexbox>
-      case "EditCreature":
-        return <Flexbox>Edited a creature {log.creature.name}</Flexbox>
-      case "DeleteCreature":
-        return <Flexbox>Deleted a creature</Flexbox>
-      case "StartCombat":
-        return <Flexbox>Started combat</Flexbox>
-      case "AddCreatureToCombat":
-        return <Flexbox>Added a creature to combat</Flexbox>
-      case "RemoveCreatureFromCombat":
-        return <Flexbox>Removed a creature from combat</Flexbox>
-      case "CombatLog":
-        return combat_log(log.log)
-      case "StopCombat":
-        return <Flexbox>Combat stopped.</Flexbox>;
-      case "CreatureLog":
-        return creature_log(log.log)
-      case "Rollback":
-        return <Flexbox>Rolled back to {log.snapshot_index}/{log.log_index}</Flexbox>
-    }
+function gameLog(log: T.GameLog): JSX.Element | null {
+  switch (log.t) {
+    case "AttributeCheckResult":
+      return <Flexbox>
+        <div>Creature ID: {log.cid}</div>
+        <div>Success? {log.success.toString()}</div>
+      </Flexbox>;
+    case "CreateFolder":
+      return <Flexbox><div>Created Folder</div><div>{M.folderPathToString(log.path)}</div></Flexbox>;
+    case "RenameFolder":
+      return <Flexbox>Renamed Folder</Flexbox>;
+    case "DeleteFolder":
+      return <Flexbox>Deleted folder {M.folderPathToString(log.path)}</Flexbox>;
+    case "DeleteFolderItem":
+      return <Flexbox>Deleted folder item in {M.folderPathToString(log.path)}</Flexbox>;
+    case "MoveFolderItem":
+      return <Flexbox>Moved folder item from {M.folderPathToString(log.path)}
+        to {M.folderPathToString(log.newPath)}</Flexbox>;
+    case "CreateItem":
+      return <Flexbox>Created item {log.item.name} in {M.folderPathToString(log.path)}</Flexbox>;
+    case "EditItem":
+      return <Flexbox>Edited item {log.item.name}</Flexbox>;
+    case "CreateNote":
+      return <Flexbox>Created note {log.note.name}</Flexbox>;
+    case "EditNote":
+      return <Flexbox>Edited note {log.name}</Flexbox>;
+    case "DeleteNote":
+      return <Flexbox>Deleted note {log.name}</Flexbox>;
+    case "CreateScene":
+      return <Flexbox>Created scene {log.scene.name}</Flexbox>;
+    case "EditScene":
+      return <Flexbox>Edited scene {log.scene.name}</Flexbox>;
+    case "DeleteScene":
+      return <Flexbox>Deleted a scene</Flexbox>;
+    case "CreateMap":
+      return <Flexbox>Created a map {log.map.name}</Flexbox>;
+    case "EditMap":
+      return <Flexbox>Edited a map {log.map.name}</Flexbox>;
+    case "DeleteMap":
+      return <Flexbox>Deleted a map</Flexbox>;
+    case "SetCreaturePos":
+      return <Flexbox>Set a creature position to {log.pos.toString()}</Flexbox>;
+    case "PathCreature":
+      const last = log.path.length > 0
+        ? log.path[log.path.length - 1].toString()
+        : "Nowhere";
+      return <Flexbox>Creature followed a path to {last}</Flexbox>;
+    case "CreateCreature":
+      return <Flexbox>Created a creature {log.creature.name}</Flexbox>;
+    case "EditCreature":
+      return <Flexbox>Edited a creature {log.creature.name}</Flexbox>;
+    case "DeleteCreature":
+      return <Flexbox>Deleted a creature</Flexbox>;
+    case "StartCombat":
+      return <Flexbox>Started combat</Flexbox>;
+    case "AddCreatureToCombat":
+      return <Flexbox>Added a creature to combat</Flexbox>;
+    case "RemoveCreatureFromCombat":
+      return <Flexbox>Removed a creature from combat</Flexbox>;
+    case "CombatLog":
+      return combat_log(log.log);
+    case "StopCombat":
+      return <Flexbox>Combat stopped.</Flexbox>;
+    case "CreatureLog":
+      return creature_log(log.log);
+    case "Rollback":
+      return <Flexbox>Rolled back to {log.snapshot_index}/{log.log_index}</Flexbox>;
   }
 }
+
 
 function combat_log(log: T.CombatLog): JSX.Element | null {
   switch (log.t) {
     case "ConsumeMovement":
       return null;
     case "ChangeCreatureInitiative":
-      return <Flexbox>Creature initiative changed</Flexbox>
+      return <Flexbox>Creature initiative changed</Flexbox>;
     case "EndTurn":
-      return <Flexbox>Creature's turn ended.</Flexbox>
+      return <Flexbox>Creature's turn ended.</Flexbox>;
     case "ForceNextTurn":
-      return <Flexbox>Forced move to next creature in combat</Flexbox>
+      return <Flexbox>Forced move to next creature in combat</Flexbox>;
     case "ForcePrevTurn":
-      return <Flexbox>Forced move to previous creature in combat</Flexbox>
+      return <Flexbox>Forced move to previous creature in combat</Flexbox>;
     case "RerollInitiative":
-      return <Flexbox>Rerolled initiative for all creatures</Flexbox>
+      return <Flexbox>Rerolled initiative for all creatures</Flexbox>;
   }
 }
 
 function creature_log(log: T.CreatureLog): JSX.Element | null {
   switch (log.t) {
     case "Damage":
-      return <Flexbox>A creature took {log.hp} damage. Rolls: {JSON.stringify(log.rolls)}</Flexbox>
+      return <Flexbox>A creature took {log.hp} damage. Rolls: {JSON.stringify(log.rolls)}</Flexbox>;
     case "Heal":
-      return <Flexbox>A creature was healed for {log.hp}. Rolls: {JSON.stringify(log.rolls)}</Flexbox>
+      return <Flexbox>
+        A creature was healed for {log.hp}. Rolls: {JSON.stringify(log.rolls)}
+      </Flexbox>;
     case "GenerateEnergy":
-      return <Flexbox>A creature received {log.energy} energy.</Flexbox>
+      return <Flexbox>A creature received {log.energy} energy.</Flexbox>;
     case "ReduceEnergy":
-      return <Flexbox>A creature's energy was reduced by {log.energy}</Flexbox>
+      return <Flexbox>A creature's energy was reduced by {log.energy}</Flexbox>;
     case "ApplyCondition":
-      return <Flexbox>A creature gained a condition</Flexbox>
+      return <Flexbox>A creature gained a condition</Flexbox>;
     case "DecrementConditionRemaining":
-      return <Flexbox>A condition was reduced in duration.</Flexbox>
+      return <Flexbox>A condition was reduced in duration.</Flexbox>;
     case "RemoveCondition":
-      return <Flexbox>A condition was removed from a creature.</Flexbox>
+      return <Flexbox>A condition was removed from a creature.</Flexbox>;
   }
 }
