ef637da4ec14d3c2102bbc1d642df4b1c1137f9b
Christopher Armstrong <radix@twistedmatrix.com>
2017-07-18 21:33:35 -0500

    show player's scene in the Players table

--- ptui/src/typescript/Players.tsx
+++ ptui/src/typescript/Players.tsx
@@ -19,23 +19,27 @@ export const Players = Comp.connect(
     ptui => ptui.app.players,
     ptui => ptui.focused_scene(),
     ],
-    (app, players, scene) => ({
-      scene,
+    (app, players, gm_scene) => ({
+      gm_scene,
       player_creatures: players.valueSeq().toArray().map(
-        player =>
-          ({
+        player => {
+          const scene = player.scene ? app.current_game.scenes[player.scene] : undefined;
+          return {
             player, id: player.player_id,
             creatures: M.filterMap(player.creatures, cid => M.getCreature(app, cid)),
-          })),
-    })
-  )
+            scene_name: scene ? scene.name : "No scene",
+          };
+        }
+      ),
+    }))
 )(
-  function Players({ scene, player_creatures, dispatch }): JSX.Element {
+  function Players({ gm_scene, player_creatures, dispatch }): JSX.Element {
     return <Table celled={true}>
       <Table.Header>
         <Table.Row>
           <Table.HeaderCell>Player</Table.HeaderCell>
           <Table.HeaderCell>Creatures</Table.HeaderCell>
+          <Table.HeaderCell>Scene</Table.HeaderCell>
           <Table.HeaderCell>Actions</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
@@ -46,8 +50,8 @@ export const Players = Comp.connect(
             if (playa.player.scene) {
               sceneButtons.push(setSceneButton(playa.id, "Remove from Scene", undefined));
             }
-            if (scene && playa.player.scene !== scene.id) {
-              sceneButtons.push(setSceneButton(playa.id, "Move to this scene", scene.id));
+            if (gm_scene && playa.player.scene !== gm_scene.id) {
+              sceneButtons.push(setSceneButton(playa.id, "Move to this scene", gm_scene.id));
             }
 
             return <Table.Row key={playa.id}>
@@ -60,6 +64,9 @@ export const Players = Comp.connect(
                     </List.Item>;
                   })}
                 </List>
+              </Table.Cell>
+              <Table.Cell>
+                {playa.scene_name}
               </Table.Cell>
               <Table.Cell>
                 <Button.Group vertical={true}>
