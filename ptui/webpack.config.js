1cc62f719361450c6a4c5b884ff5cc26b969c5db
Christopher Armstrong <radix@twistedmatrix.com>
2017-09-23 10:25:42 -0500

    get rid of ptbuild, replace it by using the html-template plugin for webpack

--- ptui/webpack.config.js
+++ ptui/webpack.config.js
@@ -1,13 +1,19 @@
-webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const { CheckerPlugin } = require('awesome-typescript-loader');
 
 const build_dir = __dirname + "/build";
 
-module.exports = {
+module.exports = function (env) {
+  if (!env.rpi_url) {
+    console.log("Please pass --env.rpi_url=http://localhost:1337/!");
+    process.exit();
+  }
+  return {
     entry: "./src/index.tsx",
     output: {
-        filename: "bundle.js",
-        path: build_dir
+      filename: "bundle.js",
+      path: build_dir
     },
     stats: "errors-only",
 
@@ -15,30 +21,42 @@ module.exports = {
     devtool: "source-map",
 
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
     },
 
     plugins: [
-        new webpack.DllReferencePlugin({
-            context: ".",
-            manifest: require("./build/vendor-manifest.json")
-        }),
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        new CheckerPlugin(),
+      new HtmlWebpackPlugin({
+        filename: build_dir + '/GM.html',
+        template: 'src/react-template.ejs',
+        rpi_url: env.rpi_url,
+        pt_app: 'GM',
+      }),
+      new HtmlWebpackPlugin({
+        filename: build_dir + '/Player.html',
+        template: 'src/react-template.ejs',
+        rpi_url: env.rpi_url,
+        pt_app: 'Player',
+      }),
+      new webpack.DllReferencePlugin({
+        context: ".",
+        manifest: require("./build/vendor-manifest.json")
+      }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new CheckerPlugin(),
     ],
     module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      rules: [
+        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+        { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
     },
 
     devServer: {
-        contentBase: build_dir,
+      contentBase: build_dir,
     }
 
     // When importing a module whose path matches one of the following, just
@@ -49,4 +67,5 @@ module.exports = {
     //     "react": "React",
     //     "react-dom": "ReactDOM"
     // },
-};
+  };
+}
