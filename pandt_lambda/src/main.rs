59fcdbdbe97d40c17ec53144ca860ce01d0a5a0f
Christopher Armstrong <227068+radix@users.noreply.github.com>
2018-11-23 14:06:12 -0600

    we need rusoto_ssm to fetch parameter store secrets

--- pandt_lambda/src/main.rs
+++ pandt_lambda/src/main.rs
@@ -2,6 +2,8 @@ use std::default::Default;
 
 use aws_lambda::start;
 use rusoto_core::Region;
+use rusoto_ssm; // I need to be able to fetch parameters so I can authenticate against AppSync
+use graphql_client; // I need to send a mutation to AppSync's GraphQL in order to notify waiting clients.
 
 fn main() {
     let region = Region::default();
