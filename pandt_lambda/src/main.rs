72e4737a2b76934ec7d17050e12b9c91251821cb
Christopher Armstrong <227068+radix@users.noreply.github.com>
2018-11-24 11:23:35 -0600

    I don't think -v works for mounting volumes in codebuild, but it's worth a try!

--- pandt_lambda/src/main.rs
+++ pandt_lambda/src/main.rs
@@ -3,9 +3,16 @@ use std::default::Default;
 use aws_lambda::start;
 use rusoto_core::Region;
 use rusoto_ssm; // I need to be able to fetch parameters so I can authenticate against AppSync
+use rusoto_cognito_identity; // I need to be able to verify cognito tokens
 use graphql_client; // I need to send a mutation to AppSync's GraphQL in order to notify waiting clients.
 
 fn main() {
+
     let region = Region::default();
+
+    // This is our cached "pandt-backend" password from SSM. It should hopefully
+    // outlive individual Lambda invocations so we don't have to make an SSM
+    // request per Lambda invocation.
+    let appsync_secret: Option<String> = None;
     start(move |()| Ok(format!("Hello {:?}!", region)))
 }
