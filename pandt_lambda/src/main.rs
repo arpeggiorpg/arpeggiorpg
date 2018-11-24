230502cb264b4a03a99694fa90eb5fbdcb04698c
Christopher Armstrong <227068+radix@users.noreply.github.com>
2018-11-24 13:12:01 -0600

    invoke rusoto_ssm to get the pandt-backend-cognito-password secret

--- pandt_lambda/src/main.rs
+++ pandt_lambda/src/main.rs
@@ -1,18 +1,22 @@
 use std::default::Default;
 
 use aws_lambda::start;
-use rusoto_core::Region;
-use rusoto_ssm; // I need to be able to fetch parameters so I can authenticate against AppSync
-use rusoto_cognito_identity; // I need to be able to verify cognito tokens
+use failure;
 use graphql_client; // I need to send a mutation to AppSync's GraphQL in order to notify waiting clients.
+use rusoto_cognito_identity; // I need to be able to verify cognito tokens
+use rusoto_core::Region;
+use rusoto_ssm::{self, Ssm}; // I need to be able to fetch parameters so I can authenticate against AppSync
 
-fn main() {
-
+fn main() -> Result<(), failure::Error> {
     let region = Region::default();
+    let ssm = rusoto_ssm::SsmClient::new(region.clone());
+    let appsync_secret_param = ssm
+        .get_parameter(rusoto_ssm::GetParameterRequest {
+            name: "pandt-backend-cognito-password".to_string(),
+            with_decryption: Some(true),
+        })
+        .sync()?;
+    let appsync_secret = appsync_secret_param.parameter.ok_or_else(|| failure::format_err!("Couldn't find pandt-backend-cognita-password parameter?"))?.value;
 
-    // This is our cached "pandt-backend" password from SSM. It should hopefully
-    // outlive individual Lambda invocations so we don't have to make an SSM
-    // request per Lambda invocation.
-    let appsync_secret: Option<String> = None;
-    start(move |()| Ok(format!("Hello {:?}!", region)))
+    Ok(start(move |()| Ok(format!("Hello {:?}!", appsync_secret))))
 }
