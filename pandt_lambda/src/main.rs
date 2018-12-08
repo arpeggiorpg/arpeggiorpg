78e97bbc5aa63a0d93b330539d40b462b63f42e0
Christopher Armstrong <227068+radix@users.noreply.github.com>
2018-12-08 12:44:50 -0600

    switch to the new official AWS lambda runtime for Rust

--- pandt_lambda/src/main.rs
+++ pandt_lambda/src/main.rs
@@ -1,13 +1,31 @@
 use std::default::Default;
 
-use aws_lambda::start;
+use lambda_runtime::{self, lambda, error::HandlerError};
 use failure;
 use graphql_client; // I need to send a mutation to AppSync's GraphQL in order to notify waiting clients.
 use rusoto_cognito_identity; // I need to be able to verify cognito tokens
 use rusoto_core::Region;
 use rusoto_ssm::{self, Ssm}; // I need to be able to fetch parameters so I can authenticate against AppSync
+use serde::Serialize;
+use serde_json::Value;
 
-fn main() -> Result<(), failure::Error> {
+fn main() {
+    // TODO: when the next lambda_runtime is released (0.2.0 probably), change
+    // this so handler is a closur (or otherwise implements the Handler trait:
+    // https://github.com/awslabs/aws-lambda-rust-runtime/blob/master/lambda-runtime/src/runtime.rs#L15)
+    // and save the `region` and `ssm` client and `appsync_secret` between
+    // requests.
+    lambda!(handler);
+}
+
+#[derive(Serialize, Debug)]
+struct Response {
+    secret: String,
+    input_event: serde_json::Value,
+}
+
+#[allow(clippy::needless_pass_by_value)]
+fn handler(event: Value, ctx: lambda_runtime::Context) -> Result<Response, HandlerError> {
     let region = Region::default();
     let ssm = rusoto_ssm::SsmClient::new(region.clone());
     let appsync_secret_param = ssm
@@ -15,14 +33,17 @@ fn main() -> Result<(), failure::Error> {
             name: "pandt-backend-cognito-password".to_string(),
             with_decryption: Some(true),
         })
-        .sync()?;
+        .sync()
+        .map_err(|_e| ctx.new_error("Couldn't get pandt-backend-cognito-password"))?;
     let appsync_secret = appsync_secret_param
         .parameter
         .ok_or_else(|| {
-            failure::format_err!("Couldn't find pandt-backend-cognito-password parameter?")
+            ctx.new_error("Couldn't find pandt-backend-cognito-password parameter?")
         })?
         .value
-        .ok_or_else(|| failure::format_err!("pandt-backend-cognito-password didn't have a value"));
-
-    Ok(start(move |()| Ok(format!("Hello {:?}!", appsync_secret))))
+        .ok_or_else(|| ctx.new_error("pandt-backend-cognito-password didn't have a value"))?;
+    Ok(Response {
+        secret: appsync_secret,
+        input_event: event,
+    })
 }
