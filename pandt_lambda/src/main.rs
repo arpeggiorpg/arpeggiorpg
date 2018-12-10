00c250e5467464430e12c2d59d9b390695e84191
Christopher Armstrong <227068+radix@users.noreply.github.com>
2018-12-09 18:44:11 -0600

    remove all interesting code from the main lambda function

--- pandt_lambda/src/main.rs
+++ pandt_lambda/src/main.rs
@@ -1,39 +1,18 @@
-use std::default::Default;
-
-use failure::{format_err, self};
-use graphql_client; // I need to send a mutation to AppSync's GraphQL in order to notify waiting clients.
 use lambda_runtime::{self, error::HandlerError, start};
-use rusoto_cognito_identity; // I need to be able to verify cognito tokens
-use rusoto_core::Region;
-use rusoto_ssm::{self, Ssm}; // I need to be able to fetch parameters so I can authenticate against AppSync
 use serde::Serialize;
 use serde_json::Value;
 
 #[derive(Serialize, Debug)]
 struct Response {
   secret: String,
-  input_event: serde_json::Value,
+  input_event: Value,
 }
 
 #[allow(clippy::needless_pass_by_value)]
 fn main() -> Result<(), failure::Error> {
-  let region = Region::default();
-  let ssm = rusoto_ssm::SsmClient::new(region.clone());
-  let appsync_secret_param = ssm
-    .get_parameter(rusoto_ssm::GetParameterRequest {
-      name: "pandt-backend-cognito-password".to_string(),
-      with_decryption: Some(true),
-    })
-    .sync()?;
-  let appsync_secret = appsync_secret_param
-    .parameter
-    .ok_or_else(|| format_err!("Couldn't find pandt-backend-cognito-password parameter?"))?
-    .value
-    .ok_or_else(|| format_err!("pandt-backend-cognito-password didn't have a value"))?;
-
   let handler = move |event: Value, _ctx: lambda_runtime::Context| -> Result<Response, HandlerError> {
     Ok(Response {
-      secret: appsync_secret.clone(),
+      secret: "foo".to_string(),
       input_event: event,
     })
   };
