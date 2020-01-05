b5dae389404192dfa3286cb043f9b62c595c24d4
Christopher Armstrong <227068+radix@users.noreply.github.com>
2020-01-05 11:34:35 -0600

    fix tests

--- ptrpi/src/main.rs
+++ ptrpi/src/main.rs
@@ -79,7 +79,7 @@ mod test {
 
   #[test]
   fn load_samplegame_yaml() {
-    ::load_app_from_path(
+    crate::load_app_from_path(
       Path::new("sample_games"),
       None,
       ModuleSource::SavedGame,
