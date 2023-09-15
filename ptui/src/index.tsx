60e80c302b181bc8a9726a2e29f1407223446af3
Christopher Armstrong <227068+radix@users.noreply.github.com>
2023-09-15 15:39:12 -0500

    got the poll loop working:
    - rewrote startPoll in terms of the zustand stores
    - moved CommonView.Main to its own file and ported it to a functional component

--- ptui/src/index.tsx
+++ ptui/src/index.tsx
@@ -1,27 +1,38 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 
-import * as CV from './CommonView';
-import * as GMView from './GMView';
-import * as PlayerView from "./PlayerView";
+import { Main } from "./Main";
+// import * as CV from "./CommonView";
+// import * as GMView from "./GMView";
+// import * as PlayerView from "./PlayerView";
 
 function getInnerComponent(component_name: string): JSX.Element {
   switch (component_name) {
-    case "GM": return <GMView.GMMain />;
-    case "Player": return <PlayerView.PlayerMain />;
-    default: throw new Error(`Unknown component ${component_name}`);
+    case "GM":
+      return <GMView.GMMain />;
+    case "Player":
+      return <PlayerView.PlayerMain />;
+    default:
+      throw new Error(`Unknown component ${component_name}`);
   }
 }
 
-function PT_renderMain(rpi_url: string, component_name: string, id: string) {
+let root;
+
+function PT_renderMain(component_name: string, id: string) {
   const el = document.getElementById(id);
-  ReactDOM.render(
-    <CV.Main rpi_url={rpi_url}>
-      {getInnerComponent(component_name)}
-    </CV.Main>,
-    el);
+  const comp = (
+    <Main>
+      Hi
+      {/* {getInnerComponent(component_name)} */}
+    </Main>
+  );
+  if (!el) {
+    console.error("where's the root!");
+    return;
+  }
+  root = createRoot(el);
+  root.render(comp);
 }
 
-const RPI_URL = import.meta.env.VITE_RPI_URL;
-
-PT_renderMain(RPI_URL, 'GM', 'react-main');
+PT_renderMain("GM", "react-main");
