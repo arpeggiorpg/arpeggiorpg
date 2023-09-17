ceb75a6782d1d6524144e40aab95ad67a7485cbb
Christopher Armstrong <227068+radix@users.noreply.github.com>
2023-09-17 10:04:34 -0500

    auto-create the player folder if it doesn't exist

--- ptui/src/PlayerView.tsx
+++ ptui/src/PlayerView.tsx
@@ -138,6 +138,12 @@ function PlayerCreatures(props: { player: T.Player }) {
 
 function PlayerNote({ player_id }: { player_id: T.PlayerID }): JSX.Element {
   const path = ["Players", player_id];
+  const folder = M.useState(s => s.getFolder(path));
+  React.useEffect(() => {
+    if (!folder) {
+      M.sendCommand({t: "CreateFolder", path});
+    }
+  }, []);
   return <CV.NoteEditor path={path} name="Scratch" disallow_rename={true} />;
 }
 
