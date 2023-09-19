6f49755da9abeb1af1b440e010d244120b2432ca
Christopher Armstrong <227068+radix@users.noreply.github.com>
2023-09-19 06:40:07 -0500

    end-to-end grpc-web!
    It turns out that protobuf-ts is MUCH better than the official "grpc-web" project.

--- ptui/src/Main.tsx
+++ ptui/src/Main.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 
 import * as M from "./Model";
-import {startPoll, RPI_URL} from "./Actions";
+import {startPoll, RPI_URL, sayHello} from "./Actions";
 
 export function Main(props: React.PropsWithChildren) {
   const fetchStatus = M.useState(s => s.fetchStatus);
   // kick off a fetch of the app
   React.useEffect(() => { startPoll(); }, []);
+  React.useEffect(() => { sayHello(); }, []);
 
   if (fetchStatus === "Unfetched") {
     return <div>Waiting for initial data from server.</div>;
