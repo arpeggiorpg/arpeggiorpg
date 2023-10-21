fa9bb2249ea3e3cdb46cc6aad48eb497d30d8a53
Christopher Armstrong <227068+radix@users.noreply.github.com>
2023-10-20 20:48:55 -0500

    use NOT NULL for sql columns

--- worker/schema.sql
+++ worker/schema.sql
@@ -1,13 +1,15 @@
+/* remember to include NOT NULL directives when creating new tables! */
+
 CREATE TABLE IF NOT EXISTS game_metadata (
-    game_id text PRIMARY KEY,
-    name text
+    game_id text PRIMARY KEY NOT NULL,
+    name text NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS user_games (
-    user_id text,
-    game_id text,
-    profile_name text,
-    role text
+    user_id text NOT NULL,
+    game_id text NOT NULL,
+    profile_name text NOT NULL,
+    role text NOT NULL
 );
 CREATE INDEX IF NOT EXISTS user_games_by_user_idx ON user_games (user_id);
