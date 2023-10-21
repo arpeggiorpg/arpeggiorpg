1396672169ac53097f37d39b033fd6d1181e80b7
Christopher Armstrong <227068+radix@users.noreply.github.com>
2023-10-21 15:34:18 -0500

    let's make sure that there is only one GM or Player per user/game

--- worker/schema.sql
+++ worker/schema.sql
@@ -9,7 +9,17 @@ CREATE TABLE IF NOT EXISTS game_metadata (
 CREATE TABLE IF NOT EXISTS user_games (
     user_id text NOT NULL,
     game_id text NOT NULL,
-    profile_name text NOT NULL,
-    role text NOT NULL
+    role text NOT NULL,
+    profile_name text NOT NULL
 );
 CREATE INDEX IF NOT EXISTS user_games_by_user_idx ON user_games (user_id);
+
+
+/* This UNIQUE constraint ensures that each *user* can only be associated to
+a game once as a GM and once as a player.
+
+(really, the only reason it includes "role" is for testing purposes. I don't
+think there are any real use-cases for a user to be both a GM and a player
+at the same time)
+*/
+CREATE UNIQUE INDEX IF NOT EXISTS user_game_role_unique ON user_games(user_id, game_id, role);
