04acfa3c0f13890f82c350374d569dfe757be4af
Christopher Armstrong <227068+radix@users.noreply.github.com>
2023-10-24 21:34:28 -0500

    refactor game storage logic into a GameStorage struct, with some new behavior:
    - game changes are now written as logs & game loading reads the latest logs (currently there are no additional snapshots beyond the first)
    - I got rid of the whole "chunk" thing because I doubt I'm going to implement that properly before CF releases the new SQLite-based storage API (which I definitely will prefer to this janky KV-based API)

--- worker/src/durablegame.rs
+++ worker/src/durablegame.rs
@@ -1,19 +1,22 @@
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-use arpeggio::types::PlayerID;
+use anyhow::{anyhow, Context};
+use arpeggio::types::{ChangedGame, Game, GameLog, PlayerID};
 use mtarp::types::{InvitationID, Role};
 use serde_json::json;
 use uuid::Uuid;
 use worker::{
-  async_trait, console_log, durable_object, js_sys, wasm_bindgen, wasm_bindgen_futures, worker_sys,
-  Env, Method, Request, Response, Result, State, WebSocket, WebSocketPair,
+  async_trait, console_error, console_log, console_warn, durable_object, js_sys, wasm_bindgen,
+  wasm_bindgen_futures, worker_sys, Env, ListOptions, Method, Request, Response, Result, State,
+  Storage, WebSocket, WebSocketPair,
 };
 
-use crate::{rust_error, wsrpi};
+use crate::{anyhow_str, rust_error, wsrpi};
 
 #[durable_object]
 pub struct ArpeggioGame {
   state: Rc<State>,
+  game_storage: Option<Rc<GameStorage>>,
   sessions: Sessions,
   ws_tokens: HashMap<Uuid, WSUser>,
 }
@@ -29,48 +32,65 @@ pub type Sessions = Rc<RefCell<Vec<WebSocket>>>;
 impl DurableObject for ArpeggioGame {
   fn new(state: State, _env: Env) -> Self {
     Self {
+      game_storage: None,
       state: Rc::new(state),
       sessions: Rc::new(RefCell::new(vec![])),
       ws_tokens: HashMap::new(),
     }
   }
 
-  async fn fetch(&mut self, mut req: Request) -> Result<Response> {
+  async fn fetch(&mut self, req: Request) -> Result<Response> {
+    let game_storage = match self.game_storage {
+      Some(ref game_storage) => game_storage.clone(),
+      None => {
+        let storage = GameStorage::load(self.state.clone()).await.map_err(rust_error)?;
+        let rc_storage = Rc::new(storage);
+        self.game_storage = Some(rc_storage.clone());
+        rc_storage
+      }
+    };
+    Ok(self.route(req, game_storage).await.map_err(rust_error)?)
+  }
+}
+
+impl ArpeggioGame {
+  async fn route(
+    &mut self, req: Request, game_storage: Rc<GameStorage>,
+  ) -> anyhow::Result<Response> {
     let path = req.path();
     console_log!("[DO] method={:?} path={path:?}", req.method());
+
     match path.split("/").collect::<Vec<_>>()[1..] {
       ["request-websocket", _game_id, role, player_id] => {
         // The worker has already authenticated & authorized the user, so we just need to store &
         // return a token.
         let token = Uuid::new_v4();
-        let role = role.parse().map_err(rust_error)?;
+        let role = role.parse()?;
         // Unfortunately, neither `worker` nor `url` have accessors for path segments that do URL
         // decoding. WTF?
-        let player_id =
-          percent_encoding::percent_decode_str(player_id).decode_utf8().map_err(rust_error)?;
+        let player_id = percent_encoding::percent_decode_str(player_id).decode_utf8()?;
         let player_id: PlayerID = PlayerID(player_id.to_string());
         console_log!("Ok what the heck is this player ID {player_id:?}");
         self.ws_tokens.insert(token, WSUser { role, player_id });
-        Response::from_json(&json!({"token": token}))
+        Response::from_json(&json!({"token": token})).map_err(anyhow_str)
       }
       ["ws", _, ws_token] => {
-        let ws_token: Uuid = ws_token.parse().map_err(rust_error)?;
+        let ws_token: Uuid = ws_token.parse()?;
         if let Some(ws_user) = self.ws_tokens.remove(&ws_token) {
           console_log!("[DO] GAME {path}");
           console_log!("[worker] WEBSOCKET");
-          let pair = WebSocketPair::new()?;
+          let pair = WebSocketPair::new().map_err(anyhow_str)?;
           let server = pair.server;
-          server.accept()?;
+          server.accept().map_err(anyhow_str)?;
 
           // We have *two* asynchronous tasks here:
           // 1. listen for messages from the client and act on the game
           // 2. listen for broadcasts from the first task and sends a message to all sessions
           // Maybe there's a simpler way to do this that doesn't involve a channel and two tasks?
 
-          // TODO: ignore poison
           self.sessions.borrow_mut().push(server.clone());
           let session = wsrpi::GameSession::new(
-            self.state.clone(),
+            game_storage,
             server,
             self.sessions.clone(),
             ws_user.role,
@@ -80,10 +100,10 @@ impl DurableObject for ArpeggioGame {
             session.run().await;
           });
 
-          Response::from_websocket(pair.client)
+          Response::from_websocket(pair.client).map_err(anyhow_str)
         } else {
           console_log!("Bad WS token {path:?}");
-          Response::error("Bad WS token", 404)
+          Response::error("Bad WS token", 404).map_err(anyhow_str)
         }
       }
       ["g", "invitations", _game_id, invitation_id] if req.method() == Method::Get => {
@@ -91,21 +111,154 @@ impl DurableObject for ArpeggioGame {
       }
       _ => {
         console_log!("Bad URL to DO: {path:?}");
-        Response::error(format!("bad URL to DO: {path:?}"), 404)
+        Response::error(format!("bad URL to DO: {path:?}"), 404).map_err(anyhow_str)
       }
     }
   }
-}
 
-impl ArpeggioGame {
-  async fn check_invitation(&self, req: Request, invitation_id: &str) -> Result<Response> {
-    let invitation_id = invitation_id.parse().map_err(rust_error)?;
+  async fn check_invitation(&self, _req: Request, invitation_id: &str) -> anyhow::Result<Response> {
+    let invitation_id = invitation_id.parse()?;
     let storage = self.state.storage();
     let invitations = storage.get("invitations").await;
     let invitations: Vec<InvitationID> = match invitations {
       Ok(r) => r,
+      Err(e) => {
+        console_error!("Error getting invitations: {e:?}");
+        vec![]
+      }
+    };
+    Response::from_json(&json!(invitations.contains(&invitation_id))).map_err(anyhow_str)
+  }
+}
+
+pub struct GameStorage {
+  state: Rc<State>,
+  current_snapshot_idx: RefCell<usize>,
+  next_log_idx: RefCell<usize>,
+  cached_game: Rc<RefCell<Game>>,
+}
+
+/// ## Storage in Durable Objects
+/// DO gives us a KV store, where the size of values is pretty significantly limited (128kB). It
+/// turns out that CloudFlare is (hopefully) going to be releasing local a local SQLite-based
+/// storage API for Durable Objects within a few months, so I am not going to bother trying to make
+/// this storage system too robust for now; it's far too onerous.
+impl GameStorage {
+
+  pub fn game(&self) -> Game {
+    self.cached_game.borrow().clone()
+  }
+
+  async fn load(state: Rc<State>) -> anyhow::Result<Self> {
+    // TODO: support muiltple snapshots? Or maybe just wait until SQLite support exists...
+
+    let (game, next_log_idx) = match state.storage().get::<String>("snapshot-0-chunk-0").await {
+      Ok(game_str) => {
+        let game = serde_json::from_str(&game_str)?;
+        Self::load_logs(state.clone(), game).await?
+      }
+      Err(e) => {
+        console_error!("ERROR: Loading game failed: {e:?}");
+        (Default::default(), 0)
+      }
+    };
+    let game_storage = Self {
+      state,
+      current_snapshot_idx: RefCell::new(0),
+      next_log_idx: RefCell::new(next_log_idx),
+      cached_game: Rc::new(RefCell::new(game)),
+    };
+    Ok(game_storage)
+  }
+
+  /// log keys are like "log-{snapshot_idx}-idx-{log_idx}"
+
+  async fn load_logs(state: Rc<State>, mut game: Game) -> anyhow::Result<(Game, usize)> {
+    // Here's another super annoying deficiency of the DO "list" API: it doesn't return an iterator,
+    // but the entire result set all at once as a javascript Map! So, we have to manually do
+    // batching to avoid loading too much stuff into memory at once.
+
+    let storage = state.storage();
+    let list_options = ListOptions::new().prefix("log-");
+    let items = storage.list_with_options(list_options).await.map_err(anyhow_str)?;
+    if items.size() == 0 {
+      return Ok((game, 0));
+    }
+    console_log!("Loading Logs: Found {} items", items.size());
+    let mut last_log_idx = 0;
+    for key in items.keys() {
+      let key = key.map_err(anyhow_str)?;
+      let value = items.get(&key);
+      let value: String = serde_wasm_bindgen::from_value(value).map_err(anyhow_str)?;
+      let key: String = serde_wasm_bindgen::from_value(key).map_err(anyhow_str)?;
+      console_log!("found a log {key:?}");
+      if let ["log", _, "idx", log_idx_str] = key.split("-").collect::<Vec<_>>()[..] {
+        let log: GameLog = serde_json::from_str(&value).map_err(|e| {
+          anyhow!("Failed parsing GameLog as JSON:\ncontent: {value:?}\nerror: {e:?}")
+        })?;
+        game = game.apply_log(&log)?;
+        last_log_idx = log_idx_str.parse()?;
+      } else {
+        console_log!("Don't know what this log is: {key:?}");
+      }
+    }
+
+    Ok((game, last_log_idx + 1))
+  }
+
+  /// Upate Game storage with changes from a changed_game. Updates the locally cached Game as well
+  /// as writing new logs to storage.
+  pub async fn store_game(&self, changed_game: ChangedGame) -> anyhow::Result<()> {
+    // NOTE: Durable Objects support "write coalescing", where you can run multiple PUT operations
+    // without awaiting and they will be merged into one "transaction". Here's the thing: I'm not
+    // sure this works with the Rust bindings. But we should try something like that. There is also
+    // a method called "transaction" which takes a closure to do this explicitly, but that's not
+    // exposed in the Rust workers API yet.
+
+    // we could probably use something more compact than JSON...
+
+    // TODO: We could occasionally produce a new snapshot... or just wait for SQLite
+    for log in changed_game.logs {
+      let serialized_log = serde_json::to_string(&log)?;
+      let key = format!(
+        "log-{:09}-idx-{:09}",
+        self.current_snapshot_idx.borrow(),
+        self.next_log_idx.borrow(),
+      );
+      console_log!("Storing log {key:?}");
+      self.state.storage().put(&key, serialized_log).await.map_err(anyhow_str)?;
+
+      *self.next_log_idx.borrow_mut() += 1;
+    }
+    *self.cached_game.borrow_mut() = changed_game.game;
+
+    Ok(())
+  }
+
+  pub async fn create_invitation(&self) -> anyhow::Result<InvitationID> {
+    let invitation_id = InvitationID::gen();
+
+    let mut invitations = self.list_invitations().await?;
+    invitations.push(invitation_id);
+    self.state.storage().put("invitations", invitations).await.map_err(anyhow_str)?;
+    Ok(invitation_id)
+  }
+
+  pub async fn list_invitations(&self) -> anyhow::Result<Vec<InvitationID>> {
+    let invitations = self.state.storage().get("invitations").await;
+    let invitations: Vec<InvitationID> = match invitations {
+      Ok(invitations) => invitations,
       Err(e) => vec![],
     };
-    Response::from_json(&json!(invitations.contains(&invitation_id)))
+    Ok(invitations)
+  }
+
+  pub async fn delete_invitation(
+    &self, invitation_id: InvitationID,
+  ) -> anyhow::Result<Vec<InvitationID>> {
+    let mut invitations = self.list_invitations().await?;
+    invitations.retain_mut(|inv| inv != &invitation_id);
+    self.state.storage().put("invitations", invitations.clone()).await.map_err(anyhow_str)?;
+    Ok(invitations)
   }
 }
--- worker/src/wsrpi.rs
+++ worker/src/wsrpi.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{cell::RefCell, rc::Rc};
 
 use anyhow::{anyhow, Context};
 use futures_util::stream::StreamExt;
@@ -9,7 +9,10 @@ use worker::{console_error, console_log, ListOptions, State, WebSocket, Websocke
 use arpeggio::types::{ChangedGame, GMCommand, Game, GameError, PlayerID, RPIGame};
 use mtarp::types::{InvitationID, RPIGameRequest, Role};
 
-use crate::{anyhow_str, durablegame::Sessions};
+use crate::{
+  anyhow_str,
+  durablegame::{GameStorage, Sessions},
+};
 
 /// A representation of a request received from a websocket. It has an ID so we can send a response
 /// and the client can match them up.
@@ -20,7 +23,7 @@ struct WSRequest {
 }
 
 pub struct GameSession {
-  state: Rc<State>,
+  game_storage: Rc<GameStorage>,
   socket: WebSocket,
   sessions: Sessions,
   role: Role,
@@ -29,9 +32,10 @@ pub struct GameSession {
 
 impl GameSession {
   pub fn new(
-    state: Rc<State>, socket: WebSocket, sessions: Sessions, role: Role, player_id: PlayerID,
+    game_storage: Rc<GameStorage>, socket: WebSocket, sessions: Sessions, role: Role,
+    player_id: PlayerID,
   ) -> Self {
-    Self { state, socket, sessions, role, player_id }
+    Self { game_storage, socket, sessions, role, player_id }
   }
 
   pub async fn run(&self) {
@@ -56,11 +60,18 @@ impl GameSession {
     if self.role == Role::GM {
       return Ok(());
     }
-    let game = self.load_game().await?;
-    if !game.players.contains_key(&self.player_id) {
-      let changed_game =
-        game.perform_gm_command(GMCommand::RegisterPlayer(self.player_id.clone()))?;
-      self.store_game(changed_game.game).await?;
+    let changed_game = {
+      let game = self.game_storage.game();
+      if !game.players.contains_key(&self.player_id) {
+        let changed_game =
+          game.perform_gm_command(GMCommand::RegisterPlayer(self.player_id.clone()))?;
+        Some(changed_game)
+      } else {
+        None
+      }
+    };
+    if let Some(changed_game) = changed_game {
+      self.game_storage.store_game(changed_game).await?;
     }
     Ok(())
   }
@@ -103,8 +114,7 @@ impl GameSession {
   async fn handle_request(&self, request: WSRequest) -> anyhow::Result<serde_json::Value> {
     // TODO: we should not need to load the game on every operation; we should instead just store an
     // Arc<RefCell(?)<Game>>  in-memory in the durable object.
-    let game = self.load_game().await?;
-
+    let game = self.game_storage.game();
     use RPIGameRequest::*;
     match (self.role, request.request) {
       (_, GMGetGame) => {
@@ -140,66 +150,19 @@ impl GameSession {
       }
 
       (Role::GM, GMGenerateInvitation) => {
-        let invitation_id = self.create_invitation().await?;
+        let invitation_id = self.game_storage.create_invitation().await?;
         Ok(serde_json::to_value(invitation_id)?)
       }
       (Role::GM, GMListInvitations) => {
-        let invitations = self.list_invitations().await?;
+        let invitations = self.game_storage.list_invitations().await?;
         Ok(serde_json::to_value(invitations)?)
       }
       (Role::GM, GMDeleteInvitation { invitation_id }) => {
-        let invitations = self.delete_invitation(invitation_id).await?;
+        let invitations = self.game_storage.delete_invitation(invitation_id).await?;
         Ok(serde_json::to_value(invitations)?)
       }
       _ => Err(anyhow!("You can't run that command as that role.")),
     }
-  }
-
-  async fn create_invitation(&self) -> anyhow::Result<InvitationID> {
-    let mut storage = self.state.storage();
-    let invitation_id = InvitationID::gen();
-
-    let invitations = storage.get("invitations").await;
-    let mut invitations: Vec<InvitationID> = match invitations {
-      Ok(r) => r,
-      Err(e) => vec![],
-    };
-    invitations.push(invitation_id);
-    storage.put("invitations", invitations).await.map_err(anyhow_str)?;
-    Ok(invitation_id)
-  }
-
-  async fn list_invitations(&self) -> anyhow::Result<Vec<InvitationID>> {
-    let invitations = self.state.storage().get("invitations").await;
-    let invitations: Vec<InvitationID> = match invitations {
-      Ok(invitations) => invitations,
-      Err(e) => vec![],
-    };
-    Ok(invitations)
-  }
-
-  async fn delete_invitation(
-    &self, invitation_id: InvitationID,
-  ) -> anyhow::Result<Vec<InvitationID>> {
-    let invitations = self.state.storage().get("invitations").await;
-    let mut invitations: Vec<InvitationID> = match invitations {
-      Ok(r) => r,
-      Err(e) => vec![],
-    };
-    invitations.retain_mut(|inv| inv != &invitation_id);
-    self.state.storage().put("invitations", invitations.clone()).await.map_err(anyhow_str)?;
-    Ok(invitations)
-  }
-
-  /// List all storage items with a prefix
-  async fn list_prefix(&self, prefix: &str) -> anyhow::Result<worker::js_sys::Map> {
-    let storage = self.state.storage();
-    let items = storage
-      .list_with_options(ListOptions::new().prefix(prefix))
-      .await
-      .map_err(anyhow_str)
-      .context(format!("Listing objects with prefix {prefix:?}"))?;
-    Ok(items)
   }
 
   async fn change_game(
@@ -207,63 +170,15 @@ impl GameSession {
   ) -> anyhow::Result<serde_json::Value> {
     let changed_game = changed_game.map_err(|e| format!("{e:?}"));
     let result = match changed_game {
-      Ok(ChangedGame { logs, game: new_game }) => {
-        self.store_game(new_game.clone()).await?;
-        let rpi_game = RPIGame(&new_game);
+      Ok(changed_game) => {
+        self.game_storage.store_game(changed_game.clone()).await?;
+        let rpi_game = RPIGame(&changed_game.game);
         self.broadcast(&json!({"t": "refresh_game", "game": rpi_game}))?;
-        Ok(logs)
+        Ok(changed_game.logs)
       }
       Err(e) => Err(format!("{e:?}")),
     };
     Ok(serde_json::to_value(result)?)
-  }
-
-  // ## Storage in Durable Objects
-  // DO gives us a KV store, where the size of values is pretty significantly limited (128kB). This
-  // requires us to break up a game into little pieces. I considered splitting up each object in a
-  // game (scenes, notes, etc) into their own key (e.g. "{snapshot_idx}-scene-{scene_id}"), but that
-  // seems like unnecessarily complexity when we can just serialize the entire game and split it
-  // into 128kB chunks, with keys like "snap-{snapshot_idx}-chunk-{chunk_idx}". dealing with smaller chunks
-  // might be less memory intensive but I doubt that will be a problem.
-  async fn load_game(&self) -> anyhow::Result<Game> {
-    let snapshot_idx = 0;
-    let items = self.list_prefix(&format!("snapshot-{snapshot_idx}-chunk-")).await?;
-
-    if items.size() == 0 {
-      // This is a new game!
-      return Ok(Default::default());
-    }
-    let mut serialized_game = String::new();
-    for key in items.keys() {
-      let key = key.map_err(anyhow_str)?;
-      let value = items.get(&key);
-      let chunk: String = serde_wasm_bindgen::from_value(value).map_err(anyhow_str)?;
-      serialized_game.push_str(&chunk);
-    }
-    let game: Game = serde_json::from_str(&serialized_game)?;
-    Ok(game)
-  }
-
-  /// Write an entire Game to Durable Object storage.
-  async fn store_game(&self, game: Game) -> anyhow::Result<()> {
-    // TODO: snapshots & logs! We could automatically look up the current snapshots in the database,
-    // but unfortunately there is no way to list the keys in the database without also retrieving
-    // the values.
-    //
-    // NOTE: Durable Objects support "write coalescing", where you can run multiple PUT operations
-    // without awaiting and they will be merged into one "transaction". Here's the thing: I'm not
-    // sure this works with the Rust bindings. But we should try something like that. There is also
-    // a method called "transaction" which takes a closure to do this explicitly, but that's not
-    // exposed in the Rust workers API yet.
-    let snapshot_idx = 0; // TODO: calculate next snapshot IDX
-    let mut storage = self.state.storage();
-    // we should probably use something other than serde_json.
-    let serialized_game = serde_json::to_string(&game)?;
-    storage
-      .put(&format!("snapshot-{snapshot_idx}-chunk-0"), serialized_game)
-      .await
-      .map_err(anyhow_str)?;
-    Ok(())
   }
 
   fn broadcast<T: Serialize>(&self, value: &T) -> anyhow::Result<()> {
